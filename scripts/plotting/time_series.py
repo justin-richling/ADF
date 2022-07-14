f1e03bd6c41c53724e2296426a3fde53a506c3f6
justin-richling <richling@ucar.edu>
2022-07-14 10:36:19 -0600

    Update time_series.py

--- scripts/plotting/time_series.py
+++ scripts/plotting/time_series.py
@@ -19,9 +19,7 @@ def time_series(adfobj):
     This script plots zonal averages.
     Compare CAM climatologies against
     other climatological data (observations or baseline runs).
-
     Description of needed inputs from ADF:
-
     case_name        -> Name of CAM case provided by "cam_case_name".
     model_rgrid_loc  -> Location of re-gridded CAM climo files provided by "cam_regrid_loc".
     data_name        -> Name of data set CAM case is being compared against,
@@ -57,47 +55,7 @@ def time_series(adfobj):
 
     case_ts_loc = adfobj.get_cam_info("cam_ts_loc", required=True)
     data_ts_loc = adfobj.get_baseline_info("cam_ts_loc", required=True)
-    
-    """
-    # Case names:
-    # NOTE: "baseline" == "reference" == "observations" will be called `base`
-    #       test case(s) == case(s) to be diagnosed  will be called `case` (assumes a list)
-    case_names = adfobj.get_cam_info('cam_case_name', required=True)  # Loop over these
-    case_climo_loc = adfobj.get_cam_info('cam_climo_loc', required=True)
 
-    # ADF variable which contains the output path for plots and tables:
-    plot_location = adfobj.plot_location
-    if not plot_location:
-        plot_location = adfobj.get_basic_info("cam_diag_plot_loc")
-    if isinstance(plot_location, list):
-        for pl in plot_location:
-            plpth = Path(pl)
-            #Check if plot output directory exists, and if not, then create it:
-            if not plpth.is_dir():
-                print(f"\t    {pl} not found, making new directory")
-                plpth.mkdir(parents=True)
-        if len(plot_location) == 1:
-            plot_loc = Path(plot_location[0])
-        else:
-            print(f"Ambiguous plotting location since all cases go on same plot. Will put them in first location: {plot_location[0]}")
-            plot_loc = Path(plot_location[0])
-    else:
-        plot_loc = Path(plot_location)
-
-    # CAUTION:
-    # "data" here refers to either obs or a baseline simulation,
-    # Until those are both treated the same (via intake-esm or similar)
-    # we will do a simple check and switch options as needed:
-    if adfobj.get_basic_info("compare_obs"):
-        data_name = "obs"  # does not get used, is just here as a placemarker
-        data_list = adfobj.read_config_var("obs_type_list")  # Double caution!
-        data_loc = adfobj.get_basic_info("obs_climo_loc", required=True)
-    else:
-        data_name = adfobj.get_baseline_info('cam_case_name', required=True)
-        data_list = data_name # should not be needed (?)
-        data_loc = adfobj.get_baseline_info("cam_climo_loc", required=True)
-    """
-    
     plot_locations = adfobj.plot_location
     
     res = adfobj.variable_defaults # dict of variable-specific plot preferences
@@ -114,17 +72,6 @@ def time_series(adfobj):
     redo_plot = adfobj.get_basic_info('redo_plot')
     print(f"\t NOTE: redo_plot is set to {redo_plot}")
 
-    #Check if the variables needed for the Taylor diags are present,
-    #If not then skip this script:
-    taylor_var_set = {'TS', 'SST', 'FSNT', 'FLNT'}
-    """if not taylor_var_set.issubset(adfobj.diag_var_list) or \
-       (not ('PRECT' in adfobj.diag_var_list) and (not ('PRECL' in adfobj.diag_var_list) or not ('PRECC' in adfobj.diag_var_list))):
-        print("\tThe Taylor Diagrams require the variables: ")
-        print("\tU, PSL, SWCF, LWCF, PRECT (or PRECL and PRECC), LANDFRAC, TREFHT, TAUX, RELHUM,T")
-        print("\tSome variables are missing so Taylor diagrams will be skipped.")
-        return
-    #End if"""
-
     #Set seasonal ranges:
     seasons = {"ANN": np.arange(1,13,1),}
     '''           "DJF": [12, 1, 2],
@@ -132,68 +79,61 @@ def time_series(adfobj):
                "MAM": [3, 4, 5],
                "SON": [9, 10, 11]}'''
 
-    # TAYLOR PLOT VARIABLES:
-    var_list = ['TS', 'SST', 'FSNT', 'FLNT','RESTOM']
+    #Time Series variables:
+    #Might be best as a list of variables in the config file?
+    #var_list = ['TS', 'SST', 'FSNT', 'FLNT', "RESTOM"]
+    var_list = ['TS', 'FSNT', 'FLNT',"RESTOM"]
 
-    #Loop over variables:
-    for var in var_list:
-
-        #Loop over model cases:
-        for case_idx, case_name in enumerate(case_names):
-
-            # LOOP OVER SEASON
-            #
-            for s in seasons:
+    #Loop over model cases:
+    for case_idx, case_name in enumerate(case_names):
+        #Loop over seasons
+        for s in seasons:
+            #Loop over variables:
+            for var in var_list:
                 #Set output plot location:
                 plot_loc = Path(plot_locations[case_idx])
-                plot_name = plot_loc / f"cam_{s}_TimeSeries_Mean.{plot_type}"
-                print(f"\t - Plotting Time Series, {s}")
+                plot_name = plot_loc / f"{var}_{s}_TimeSeries_Mean.{plot_type}"
+
+                #Check if plot output directory exists, and if not, then create it:
+                if not plot_loc.is_dir():
+                    print(f"    {plot_loc} not found, making new directory")
+                    plot_loc.mkdir(parents=True)
+                print(f"\t - Plotting Time Series, {var} {s}")
 
                 # Check redo_plot. If set to True: remove old plot, if it already exists:
                 if (not redo_plot) and plot_name.is_file():
                     continue
                 elif (redo_plot) and plot_name.is_file():
                     plot_name.unlink()
-
-                """ # hold the data in a DataFrame for each case
-                # variable | correlation | stddev ratio | bias
-                df_template = pd.DataFrame(index=var_list, columns=['corr', 'ratio', 'bias'])
-                result_by_case = {cname: df_template.copy() for cname in case_names}
-                #"""
-                """# LOOP OVER VARIABLES
-                #
-                for v in var_list:
-                    base_x = _retrieve(adfobj, v, data_name, data_loc) # get the baseline field
-                    for casenumber, case in enumerate(case_names):     # LOOP THROUGH CASES
-                        case_x = _retrieve(adfobj, v, case, case_climo_loc[casenumber])
-                        # ASSUMING `time` is 1-12, get the current season:
-                        case_x = case_x.sel(time=seasons[s]).mean(dim='time')
-                        result_by_case[case].loc[v] = taylor_stats_single(case_x, base_x)"""
                     
-                
-                ts_aave_case,ts_aave_base,yrs = get_data(case_ts_loc,data_ts_loc,var)
+                if var == "RESTOM":
+                    ave_case,ave_base,unit,yrs_case,yrs_base = get_restom_data(case_ts_loc,data_ts_loc)
+                else:
+                    ave_case,ave_base,unit,yrs_case,yrs_base = get_data(case_ts_loc,data_ts_loc,var)
 
-                vals_case = [ts_aave_case.sel(time=i).mean() for i in yrs]
-                vals_base = [ts_aave_base.sel(time=i).mean() for i in yrs]
+                vals_case = [ave_case.sel(time=i).mean() for i in yrs_case]
+                vals_base = [ave_base.sel(time=i).mean() for i in yrs_base]
                     
-                #
-                # -- PLOTTING (one per season) --
-                #
                 fig, ax = plt.subplots(figsize=(15,6))
                 plt.subplots_adjust(wspace=0.52,hspace=0.2)
                         
-                ax = ts_plot(ax,case_names,data_name,vals_case,vals_base)    
+                ax = ts_plot(ax,case_names,data_name,vals_case,vals_base,unit,yrs_case,yrs_base)    
 
-                # add text with variable names:
-                #txtstrs = [f"{i+1} - {v}" for i, v in enumerate(var_list)]
-                fig.legend(loc='upper left', bbox_to_anchor=(0.33, 0.02))
-                #fig.text(0.9, 0.9, "\n".join(txtstrs), va='top')
-                fig.savefig(plot_name, bbox_inches='tight')
-                print(f"\t Time Series: completed {s}. \n\t File: {plot_name}")
+                #Set legend for case plots
+                fig.legend(bbox_to_anchor=(-0.1, .88, 1., .102), loc="right",
+                    ncol=1, borderaxespad=0.0)
+
+                #Set Main title for subplots:
+                fig.suptitle(f"Time Series: {var} - {s}", fontsize=15,horizontalalignment = 'left', x=fig.subplotpars.left,y=0.92)
+                fig.savefig(plot_name, bbox_inches='tight', dpi=300)
+                #print(f"\t Time Series: completed {s}. \n\t File: {plot_name}")
+            
+            #End for (variables loop)
+        #End for (seasons loop)
+    #End for (case names loop)
 
     #Notify user that script has ended:
     print("  ...Time Series have been generated successfully.")
-
 
 #
 # Helpers
@@ -208,29 +148,44 @@ def _load_dataset(fils):
         sfil = str(fils[0])
         return xr.open_dataset(sfil)
 
-def get_data(case_ts_loc,data_ts_loc,var):#var='TS'
-    fils_case = sorted(list(Path(case_ts_loc[0]).glob(f"*v{var}*.nc")))
-    ts_case = xr.open_mfdataset(fils_case)[f'{var}'].compute()
-    w_case = np.cos(np.radians(ts_case.lat))  # area weighting
-    ts_aave_case = ts_case.weighted(w_case).mean(dim=("lat","lon"))
+def get_data(case_ts_loc,data_ts_loc,var):
+    ave_case,unit,yrs_case = _data_calcs(case_ts_loc[0],var)
+    ave_base,unit,yrs_base = _data_calcs(data_ts_loc,var)
 
-    fils_base = sorted(list(Path(data_ts_loc).glob(f"*{var}*.nc")))
-    ts_base = xr.open_mfdataset(fils_base)[var].compute()
-    w_base = np.cos(np.radians(ts_base.lat))  # area weighting
-    ts_aave_base = ts_base.weighted(w_base).mean(dim=("lat","lon"))
+    return ave_case,ave_base,unit,yrs_case,yrs_base
 
-    woo = [val.item().strftime()[0:4] for _,val in enumerate(ts_case["time"])]
-    yrs = np.unique(woo)
-    return ts_aave_case,ts_aave_base,yrs
+def get_restom_data(case_ts_loc,data_ts_loc):
+    ave_case_FSNT,unit,yrs_case = _data_calcs(case_ts_loc[0],'FSNT')
+    ave_base_FSNT,unit,yrs_base = _data_calcs(data_ts_loc,'FSNT')
 
-def ts_plot(ax, case_name, data_name, vals_case, vals_base, yrs):
-    for i, c in enumerate(case_name):
-        labels = [data_name, c]
+    ave_case_FLNT,_,_ = _data_calcs(case_ts_loc[0],"FLNT")
+    ave_base_FLNT,_,_ = _data_calcs(data_ts_loc,"FLNT")
+
+    restom_case = ave_case_FSNT - ave_case_FLNT
+    restom_base = ave_base_FSNT - ave_base_FLNT
+
+    return restom_case,restom_base,unit,yrs_case,yrs_base
+
+def ts_plot(ax, case_names, data_name, vals_case, vals_base, unit, yrs_case, yrs_base):
+    for _,val in enumerate(case_names):
+        ax.plot(yrs_case, vals_case, c="b", label=val)
+
+    ax.plot(yrs_base, vals_base, "--", c="orange",label=data_name)
+    ax.set_ylabel(unit,fontsize=15,labelpad=20)
+    ax.set_xlabel("Years",fontsize=15,labelpad=20)
+    ax.set_xticks(yrs_case, yrs_case, rotation=45, ha='right', rotation_mode='anchor')
+    ax.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
     
-    ax.plot(yrs, vals_case, c="b", label=case_name)
-    ax.scatter(yrs, vals_base, marker="*",c="orange", s=100,label=data_name)
-
-    ax.set_xticks(yrs)
-
-    ax.set_xlabel("Time")
     return ax
+
+def _data_calcs(ts_loc,var):
+    fils = sorted(list(Path(ts_loc).glob(f"*{var}*.nc")))
+    ts = xr.open_mfdataset(fils)[var].compute()
+    w= np.cos(np.radians(ts.lat))  # area weighting
+    ave  = ts.weighted(w).mean(dim=("lat","lon")) # global averaging
+    unit = ts.units
+    
+    dtimes = [val.item().strftime()[0:4] for _,val in enumerate(ts["time"])]
+    yrs = np.unique(dtimes)
+
+    return ave,unit,yrs
