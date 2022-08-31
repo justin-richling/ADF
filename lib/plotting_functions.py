5072b8dec63f358263d1bf7ebf7f9c9622b9381f
justin-richling <richling@ucar.edu>
2022-08-31 10:37:31 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1433,9 +1433,9 @@ def plot_zonal_mean_and_save(wks, case_nickname, base_nickname,
 
         zonal_plot(adata['lat'], diff, ax=ax[1], color="k")
         #ax[1].set_title("Test - Baseline", loc='left', fontsize=10)
-        ax[1].text(-300.5, 0.15, "Test", fontsize=10,color="#1f77b4")
-        ax[1].text(0.055, 0.15, " - ", fontsize=10)
-        ax[1].text(0.06,0.15,"Baseline", fontsize=10, color="#ff7f0e")
+        ax[1].text(-80, 0.25, "Test", fontsize=10,color="#1f77b4")
+        ax[1].text(-70, 0.25, " - ", fontsize=10)
+        ax[1].text(-60,0.25,"Baseline", fontsize=10, color="#ff7f0e")
 
         for a in ax:
             try:
