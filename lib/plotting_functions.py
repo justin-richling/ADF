46202264c7d8b4db091bd5779594af0e864898c7
justin-richling <richling@ucar.edu>
2022-08-31 09:58:52 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1404,7 +1404,7 @@ def plot_zonal_mean_and_save(wks, case_nickname, base_nickname,
         from matplotlib.lines import Line2D
         #line = Line2D([0], [0], label=f"Test: {case_nickname} - years: {case_climo_yrs[0]}-{case_climo_yrs[-1]}",
         #                color="#1f77b4")
-        line = Line2D([0], [0], label=f"\033[1mTest\033[0m: {case_nickname} - years: {case_climo_yrs[0]}-{case_climo_yrs[-1]}",
+        line = Line2D([0], [0], label=r'$\b"Test"$'+": {case_nickname} - years: {case_climo_yrs[0]}-{case_climo_yrs[-1]}",
                         color="#1f77b4")
     
         line2 = Line2D([0], [0], label=f"Baseline: {base_nickname} - years: {baseline_climo_yrs[0]}-{baseline_climo_yrs[-1]}",
