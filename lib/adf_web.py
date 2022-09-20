33999cdc8bfd3c9f9ead5640aef76f434686bcc2
justin-richling <richling@ucar.edu>
2022-09-20 09:11:18 -0600

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -297,57 +297,6 @@ class AdfWeb(AdfObs):
 
     #########
 
-    def make_multi_plots(self):
-        """
-        Generate new multi-plot image from seperate images.
-        """
-        from glob import glob
-        from PIL import Image
-        from math import ceil, floor
-
-        PATH = "./"
-
-        frame_width = 1920
-        images_per_row = 6
-        padding = 2
-
-        vars = self.diag_var_list
-        seasons = ["ANN","DJF","MAM","JJA","SON"]
-
-        for season in seasons:
-            for var in vars:
-                print(season,var)
-                images = glob(f"*{var}*{season}*.png")
-                print(var,season,images)
-
-                img_width, img_height = Image.open(images[0]).size
-                sf = (frame_width-(images_per_row-1)*padding)/(images_per_row*img_width)       #scaling factor
-                scaled_img_width = ceil(img_width*sf)                   #s
-                scaled_img_height = ceil(img_height*sf)
-
-                number_of_rows = ceil(len(images)/images_per_row)
-                frame_height = ceil(sf*img_height*number_of_rows) 
-
-                new_im = Image.new('RGB', (frame_width, frame_height),color="white")
-
-                i,j=0,0
-                for num, im in enumerate(images):
-                    if num%images_per_row==0:
-                        i=0
-                    im = Image.open(im)
-                    #Here I resize my opened image, so it is no bigger than 100,100
-                    im.thumbnail((scaled_img_width,scaled_img_height))
-                    #Iterate through a 4 by 4 grid with 100 spacing, to place my image
-                    y_cord = (j//images_per_row)*scaled_img_height
-                    new_im.paste(im, (i,y_cord))
-                    #print(i, y_cord)
-                    i=(i+scaled_img_width)+padding
-                    j+=1
-
-                new_im.save(f"out_{season}_{var}.jpg", "JPEG", quality=80, optimize=True, progressive=True)
-
-
-
     def create_website(self):
 
         """
