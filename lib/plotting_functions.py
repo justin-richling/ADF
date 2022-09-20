e64eafe2efd60452e5b137b25e2df637ae52bc55
justin-richling <richling@ucar.edu>
2022-09-20 13:19:07 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1784,7 +1784,7 @@ def make_multi_plots(adfobj,case_name,im_path,var_list,seasons,plot_type):
         for var in var_list:
             #print(season,var,im_path)
             images = glob(str(im_path / f"*{var}*{season}*_all_case.png"))
-            #print("images????",images)
+            print("images????",images)
 
             img_width, img_height = Image.open(images[0]).size
             sf = (frame_width-(images_per_row-1)*padding)/(images_per_row*img_width)       #scaling factor
