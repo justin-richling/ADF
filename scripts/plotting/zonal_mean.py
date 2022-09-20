977d96d9b88e38ab3fbb1c77e8c43557e5193ca4
justin-richling <richling@ucar.edu>
2022-09-20 12:55:25 -0600

    Update zonal_mean.py

--- scripts/plotting/zonal_mean.py
+++ scripts/plotting/zonal_mean.py
@@ -284,6 +284,7 @@ def zonal_mean(adfobj):
                     
                     #Check to see if we should save the test case subplot for multi-case full plot
                     if var in multi_var_list:
+                        print("*** Grabbing test case subplot for multi-case plots, boi! ***")
                         multi_save = True
                     
                     # Check redo_plot. If set to True: remove old plot, if it already exists:
