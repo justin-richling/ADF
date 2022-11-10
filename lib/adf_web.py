c1a6868deee8cf665b8ef62960b48530f94337c3
justin-richling <richling@ucar.edu>
2022-11-10 10:03:17 -0700

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -768,7 +768,7 @@ class AdfWeb(AdfObs):
             #loop over cases:
 
             case_sites2 = []
-            for idx, case_name in enumerate(case_names):
+            for idx, case_name in enumerate(case_names[:-1]):
                 #Check if case name is present in plot
                 if case_name in self.__case_web_paths:
                     #Extract website directory:
