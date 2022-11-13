6ec9e6ccbd67866b412f9e3ca380dfa9bceddb6e
justin-richling <richling@ucar.edu>
2022-11-13 12:58:38 -0700

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -789,10 +789,10 @@ class AdfWeb(AdfObs):
             #End if
                 #End if
             #End for (model case loop)
-
+            wks = "/glade/scratch/richling/adf-output/multi-case/b.cesm3_cam058_mom_e.B1850MOM.ne30_L32_t061.camdev_cice5.009_vs_016_022c_024_026a_026b/diag-plot/b.cesm3_cam058_mom_e.B1850MOM.f09_L32_t061.cam6_cice5.016_30_40_vs_b.cesm3_cam058_mom_c.B1850WscMOM.ne30_L58_t061.009_30_40/mulit_plot.png"
             #Create multi-case site:
             multi_plots = {"Tables": "html_table/mean_tables.html",
-                           "LatLon": "html_table/mean_tables.html"}
+                           "LatLon": wks}
             main_title = "ADF Diagnostics"
             main_tmpl = jinenv.get_template('template_multi_case_index.html')
             main_rndr = main_tmpl.render(title=main_title,
