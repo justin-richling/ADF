f0193c8443c72cc3fc9493a3b951696463166b73
justin-richling <richling@ucar.edu>
2022-11-13 13:07:54 -0700

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -789,7 +789,7 @@ class AdfWeb(AdfObs):
             #End if
                 #End if
             #End for (model case loop)
-            wks = "/glade/scratch/richling/adf-output/multi-case/b.cesm3_cam058_mom_e.B1850MOM.ne30_L32_t061.camdev_cice5.009_vs_016_022c_024_026a_026b/diag-plot/b.cesm3_cam058_mom_e.B1850MOM.f09_L32_t061.cam6_cice5.016_30_40_vs_b.cesm3_cam058_mom_c.B1850WscMOM.ne30_L58_t061.009_30_40/mulit_plot.png"
+            wks = "b.cesm3_cam058_mom_e.B1850MOM.ne30_L32_t061.camdev_cice5.009_vs_016_022c_024_026a_026b/diag-plot/b.cesm3_cam058_mom_e.B1850MOM.f09_L32_t061.cam6_cice5.016_30_40_vs_b.cesm3_cam058_mom_c.B1850WscMOM.ne30_L58_t061.009_30_40/mulit_plot.png"
             #Create multi-case site:
             multi_plots = {"Tables": "html_table/mean_tables.html",
                            "LatLon": wks}
