c6000bad261022c9fdee23c8a5162578e23c9b3d
justin-richling <richling@ucar.edu>
2022-11-15 12:30:54 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1641,7 +1641,7 @@ def multi_plots(wks, ptype, case_names, nicknames, multi_dict):
     #These will need to be changed if the figsize changes!
     hspace_dict = {
     **dict.fromkeys([2, 3], 0), 
-    **dict.fromkeys([4,5,6], -1.15),
+    **dict.fromkeys([4,5,6], -0.99),
     **dict.fromkeys([7,8,9], -0.85), 
     **dict.fromkeys([10,11,12], -0.65),
     **dict.fromkeys([13,14,15], 0.35),
@@ -1659,6 +1659,7 @@ def multi_plots(wks, ptype, case_names, nicknames, multi_dict):
     #ncols = int(np.sqrt(nplots)) + 1
     ncols = 3
     nplots = len(nicknames)
+    print("\n",hspace_dict[nplots],"\n")
     nrows = int(np.ceil(nplots/ncols))
     if nrows < 2:
         nrows = 2
