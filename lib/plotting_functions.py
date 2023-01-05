132c122095808716e20972476b2ef8691494f24b
justin-richling <richling@ucar.edu>
2023-01-05 09:15:55 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1737,6 +1737,7 @@ def multi_plots(wks, ptype, case_names, nicknames, multi_dict):
                     
                 plt.subplots_adjust(wspace=0.3, hspace=hspace) #hspace_dict[nplots]
                 print("I AM TRYING TO SAVE A MULTI CASE PLOT, HELP ME OUT HERE!!!!!\n")
+                print(wks+f"{var}_{season}_{ptype}_multi_plot.png")
                 fig.savefig(wks+f"{var}_{season}_{ptype}_multi_plot.png", bbox_inches='tight', dpi=300)#, dpi=300
 
                 #Close plots:
