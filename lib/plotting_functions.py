46302ed7e11dce9401068febf61a559c1bfd7b7b
justin-richling <richling@ucar.edu>
2023-01-18 18:51:04 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1679,7 +1679,7 @@ def multi_latlon_plots(wks, ptype, case_names, nicknames, multi_dict, adfobj):
                 # Check redo_plot. If set to True: remove old plot, if it already exists:
                 redo_plot = adfobj.get_basic_info('redo_plot')
                 print("redo_plot",redo_plot)
-                print(Path(wks / f"{var}_{season}_{ptype}_multi_plot.png").is_file())
+                #print(Path(wks / f"{var}_{season}_{ptype}_multi_plot.png").is_file())
                 if (not redo_plot) and Path(wks / f"{var}_{season}_{ptype}_multi_plot.png").is_file():
                     print("Looks like that file exists partner, giddy up!\n")
                     #Continue to next iteration:
