87b5b4ca1d0789f7073309ed574b9bcf66d34329
justin-richling <richling@ucar.edu>
2023-01-19 13:49:18 -0700

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -525,6 +525,7 @@ class AdfWeb(AdfObs):
                 #Note:  Need to use data name instead of case name for tables.
                 if len(case_names) >1:
                     table_html_info[web_data.name] = web_data.html_file[0].name
+                    print("web_data.name",web_data.name,"\n")
                     multi_table_html_info[web_data.name] = str(web_data.html_file[0].name).replace("main_website",f"{web_data.name}/website")
                 else:
                     table_html_info[web_data.name] = web_data.html_file.name
