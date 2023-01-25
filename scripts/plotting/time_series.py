12eb3b6be9b4e4a937a0c0610bf5a9c206f63332
justin-richling <richling@ucar.edu>
2023-01-25 10:08:40 -0700

    Update time_series.py

--- scripts/plotting/time_series.py
+++ scripts/plotting/time_series.py
@@ -236,7 +236,8 @@ def time_series(adfobj):
         # - RESTOM
         #if ("FSNT" in ts_var_list) and ("FLNT" in ts_var_list):
         if all(value in ts_var_list for value in ["FSNT","FLNT"]):
-        #if any((match := item) in ts_var_list for item in ["FSNT","FLNT"]):
+            print("DID IT MAKE IT TO THIS POINT, AHHHHHHHHHHHH\n")
+            #if any((match := item) in ts_var_list for item in ["FSNT","FLNT"]):
    
             if season == "ANN":
                 var = "RESTOM"
