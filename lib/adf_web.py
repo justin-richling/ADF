ee9a5371c36438b62de676cfd34b43dfd29b5883
justin-richling <richling@ucar.edu>
2023-03-02 11:34:56 -0700

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -418,6 +418,15 @@ class AdfWeb(AdfObs):
         multi_case_plots = self.read_config_var('multi_case_plots')
         #Grab all variables for each multi-cacse plot type
         mvars = [i for sub in [multi_case_plots[x] for x in multi_case_plots] for i in sub]
+        print("mvars:",mvars,"\n")
+
+
+        mvars = []
+        for sub in [multi_case_plots[x] for x in multi_case_plots]:
+            for i in sub:
+                mvars.append(i)
+
+
 
         #Set plot type html dictionary (for Jinja templating):
         plot_type_html = OrderedDict()
