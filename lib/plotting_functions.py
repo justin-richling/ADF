e608067787b70d3d10c8c340c99faf9c44021ab3
justin-richling <richling@ucar.edu>
2023-03-02 10:51:13 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1775,7 +1775,7 @@ def multi_latlon_plots(wks, ptype, case_names, nicknames, multi_dict, web_catego
                     """
 
                     adfobj.add_website_data(wks / file_name, file_name, case_names[0], plot_ext="global_latlon_map",
-                                                        category=web_category, season=season, plot_type="LatLon")
+                                                        category=web_category, season=season, plot_type="LatLon",multi_case=True)
 
                     #Close plots:
                     plt.close()
