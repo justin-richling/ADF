c640fa46191895f1505f98fe0fd034e28f9b6628
justin-richling <richling@ucar.edu>
2023-03-03 17:23:40 -0700

    Update qbo.py

--- scripts/plotting/qbo.py
+++ scripts/plotting/qbo.py
@@ -69,8 +69,8 @@ def qbo(adfobj):
     # Check redo_plot. If set to True: remove old plots, if they already exist:
     if (not redo_plot) and plot_loc_ts.is_file() and plot_loc_amp.is_file():
         #Add already-existing plot to website (if enabled):
-        adfobj.add_website_data(plot_loc_ts, "QBO", None, season="QBOts") #multi_case=True
-        adfobj.add_website_data(plot_loc_amp, "QBO", None, season="QBOamp") #multi_case=True
+        adfobj.add_website_data(plot_loc_ts, "QBO", None, season="QBOts", multi_case=True) #multi_case=True
+        adfobj.add_website_data(plot_loc_amp, "QBO", None, season="QBOamp", multi_case=True) #multi_case=True
 
         #Continue to next iteration:
         return
@@ -146,7 +146,7 @@ def qbo(adfobj):
     fig.savefig(plot_loc_ts, bbox_inches='tight', facecolor='white')
 
     #Add plot to website (if enabled):
-    adfobj.add_website_data(plot_loc_ts, "QBO", None, season="QBOts") #multi_case=True
+    adfobj.add_website_data(plot_loc_ts, "QBO", None, season="QBOts", multi_case=True) #multi_case=True
 
     #-----------------
 
@@ -173,7 +173,7 @@ def qbo(adfobj):
     fig.savefig(plot_loc_amp, bbox_inches='tight', facecolor='white')
 
     #Add plot to website (if enabled):
-    adfobj.add_website_data(plot_loc_amp, "QBO", None, season="QBOamp") #multi_case=True
+    adfobj.add_website_data(plot_loc_amp, "QBO", None, season="QBOamp", multi_case=True) #multi_case=True
 
     #-------------------
 
