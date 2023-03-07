3b34b41305209ad85212f7ca0842ab47b9f09e34
justin-richling <richling@ucar.edu>
2023-03-07 09:53:06 -0700

    Update cam_taylor_diagram.py

--- scripts/plotting/cam_taylor_diagram.py
+++ scripts/plotting/cam_taylor_diagram.py
@@ -674,7 +674,7 @@ def taylor_plot_finalize(wks, casenames, casecolors, syear_cases, eyear_cases, n
 
     if multi:
         for case_idx, (s, c) in enumerate(zip([casenames], [casecolors])):
-            print(case_pos-((case_idx+1)*height_of_lines),"\n")
+            #print(case_pos-((case_idx+1)*height_of_lines),"\n")
             #text = wks.text(0.99, 0.58+bottom_of_text + n*height_of_lines, f"{s}  yrs: {syear_cases[case_idx]}-{eyear_cases[case_idx]}",
             text = wks.text(0.99, case_pos-((case_idx+1)*height_of_lines), f"{s}  yrs: {syear_cases}-{eyear_cases}",
                             color=c, va='top', transform=wks.transAxes, fontsize=10)
