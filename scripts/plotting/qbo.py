45ae1600df54c9df772309a4ae1a96b49726e45e
justin-richling <richling@ucar.edu>
2023-03-14 12:42:34 -0600

    Update qbo.py

--- scripts/plotting/qbo.py
+++ scripts/plotting/qbo.py
@@ -167,8 +167,8 @@ def qbo(adfobj):
                                             x1[icase+1],x2[icase+1],y1[icase+1],y2[icase+1], 
                                             case_nicknames[icase])
                     
-                    ax_m = plotqbotimeseries(fig_m, casedat_5S_5N[-1],minny,
-                                                x1[-1],x2[-1],y1[-1],y2[-1], 
+                    ax_m = plotqbotimeseries(fig_m, casedat_5S_5N[ncases-1],minny,
+                                                x1[ncases-1],x2[ncases-1],y1[ncases-1],y2[ncases-1], 
                                                 base_nickname)
 
                     ax_m = plotcolorbar(fig_m, x1[0]+0.2, x2[2]-0.2,y1[1]-0.035,y1[1]-0.03)
