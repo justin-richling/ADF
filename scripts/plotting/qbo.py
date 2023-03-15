2f319b024eade61539536440859907ac6144df08
justin-richling <richling@ucar.edu>
2023-03-15 16:12:36 -0600

    Update qbo.py

--- scripts/plotting/qbo.py
+++ scripts/plotting/qbo.py
@@ -157,7 +157,7 @@ def qbo(adfobj):
                                                 x1[2],x2[2],y1[2],y2[2], 
                                                 base_nickname)
 
-                    ax_m = plotcolorbar(fig_m, x1[0]+0.2, x2[2]-0.2,y1[3]-0.035,y1[3]-0.03)
+                    ax_m = plotcolorbar(fig_m, x1[0]+0.2, x2[2]-0.2,y1[2]-0.035,y1[2]-0.03)
                     #Save figure to file:
                     fig_m.savefig(plot_loc_ts, bbox_inches='tight', facecolor='white')
 
