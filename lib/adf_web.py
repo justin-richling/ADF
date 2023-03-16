668c9e2c16a67d505f2d43bea236b88bc0771ff2
justin-richling <richling@ucar.edu>
2023-03-16 13:26:27 -0600

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -643,7 +643,7 @@ class AdfWeb(AdfObs):
 
                                     if season not in multi_plot_html_info[ptype][category][var]:
                                         p = f"plot_page_multi_case_{var}_{season}_{ptype}_Mean.html"
-                                        print("multi-case multi-plot p",p,"\n")
+                                        #print("multi-case multi-plot p",p,"\n")
                                         multi_plot_html_info[ptype][category][var][season] = p
                                     #End if
                                 #End for
@@ -1188,7 +1188,7 @@ class AdfWeb(AdfObs):
                     #if not multi_case_plots:
                     #if 1==1:
                     #if ptype != "LatLon":
-                    print("ext",ext,"\n")
+                    #print("ext",ext,"\n")
                     if ext not in multi_case_dict:
                         print(f"{ext} made it through\n")
                         #var = web_data.name
