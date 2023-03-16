d4f02026638cca7683f8c4cca36881f9e627a5fc
justin-richling <richling@ucar.edu>
2023-03-16 08:49:53 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1712,7 +1712,7 @@ def multi_latlon_plots(wks, ptype, case_names, nicknames, multi_dict, web_catego
                                             subplot_kw={"projection": proj})
 
                     #Set figure title
-                    plt.suptitle(f'All Case Comparison (Test - Baseline)  {var}: {season}\n', fontsize=16,y=0.4)#y=0.325
+                    plt.suptitle(f'All Case Comparison (Test - Baseline)  {var}: {season}\n', fontsize=16,y=0.225)#y=0.325
                     axs[0,1].set_title("$\mathbf{Baseline}:$"+f'{nicknames[1]}\n', fontsize=12)
                     
                     normfunc,_ = use_this_norm()
