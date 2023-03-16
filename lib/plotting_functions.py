26cfb8edbf21be70b9dcd2a512a971094bd34af5
justin-richling <richling@ucar.edu>
2023-03-16 15:17:01 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1686,7 +1686,7 @@ def multi_latlon_plots(wks, ptype, case_names, nicknames, multi_dict, web_catego
     print("number of rows:",nrows,"\n")
 
     if nrows == 1:
-        y_title = 0.2
+        y_title = 0.25
     else:
         y_title = 0.3
 
