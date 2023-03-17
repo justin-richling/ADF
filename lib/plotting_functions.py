e70521ad214fa70f10a0cd0b67ba25009aceb580
justin-richling <richling@ucar.edu>
2023-03-17 15:16:47 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1699,86 +1699,87 @@ def multi_latlon_plots(wks, ptype, case_names, nicknames, multi_dict, web_catego
     lat_formatter = LatitudeFormatter(number_format='0.0f',
                                         degree_symbol='')
     for var in multi_dict.keys():
-        for case in multi_dict[var].keys():
-            for season in multi_dict[var][case].keys():
-                file_name = f"{var}_{season}_{ptype}_multi_plot.png"
-                if (not redo_plot) and Path(wks / file_name).is_file():
-                    #Continue to next iteration:
-                    continue
-                elif (redo_plot) or (not Path(wks / file_name).is_file()):
-                    fig_width = 15
-                    fig_height = 15+(3*nrows) #try and dynamically create size of fig based off number of cases (therefore rows)
-                    fig, axs = plt.subplots(nrows=nrows,ncols=ncols,figsize=(fig_width,fig_height), facecolor='w', edgecolor='k',
-                                            sharex=True,
-                                            sharey=True,
-                                            subplot_kw={"projection": proj})
+        if (adfobj.compare_obs) and (var in adfobj.var_obs_dict):
+            for case in multi_dict[var].keys():
+                for season in multi_dict[var][case].keys():
+                    file_name = f"{var}_{season}_{ptype}_multi_plot.png"
+                    if (not redo_plot) and Path(wks / file_name).is_file():
+                        #Continue to next iteration:
+                        continue
+                    elif (redo_plot) or (not Path(wks / file_name).is_file()):
+                        fig_width = 15
+                        fig_height = 15+(3*nrows) #try and dynamically create size of fig based off number of cases (therefore rows)
+                        fig, axs = plt.subplots(nrows=nrows,ncols=ncols,figsize=(fig_width,fig_height), facecolor='w', edgecolor='k',
+                                                sharex=True,
+                                                sharey=True,
+                                                subplot_kw={"projection": proj})
 
-                    #Set figure title
-                    plt.suptitle(f'All Case Comparison (Test - Baseline)  {var}: {season}\n', fontsize=16, y=y_title)#y=0.325 y=0.225
-                    
-                    normfunc,_ = use_this_norm()
-
-                    count = 0
-                    img = []
-                    titles = []
-                    for r in range(0,nrows):
-                        for c in range(0,ncols):
-                            if count < nplots:
-                                mdlfld = multi_dict[var][case_names[count]][season]["diff_data"]
-                                lat = mdlfld['lat']
-                                mwrap, lon = add_cyclic_point(mdlfld, coord=mdlfld['lon'])
-
-                                # mesh for plots:
-                                lons, lats = np.meshgrid(lon, lat)
-
-                                levelsdiff = multi_dict[var][case_names[count]][season]["vres"]["diff_contour_range"]
-                                levelsdiff = np.arange(levelsdiff[0],levelsdiff[1]+levelsdiff[-1],levelsdiff[-1])
-
-                                # color normalization for difference
-                                if (np.min(levelsdiff) < 0) and (0 < np.max(levelsdiff)):
-                                    normdiff = normfunc(vmin=np.min(levelsdiff), vmax=np.max(levelsdiff), vcenter=0.0)
-                                else:
-                                    normdiff = mpl.colors.Normalize(vmin=np.min(levelsdiff), vmax=np.max(levelsdiff))
-
-                                cmap = multi_dict[var][case_names[count]][season]["vres"]['diff_colormap']
-
-                                img.append(axs[r,c].contourf(lons, lats, mwrap, levels=levelsdiff, 
-                                                cmap=cmap, norm=normdiff, 
-                                                transform=proj))
-
-                                #Set individual plot titles (case name/nickname)
-                                titles.append(axs[r,c].set_title("$\mathbf{Test}:$"+f" {nicknames[0][count]}",loc='left',fontsize=8))
-                                titles.append(axs[r,c].set_title("$\mathbf{Baseline}:$"+f" {nicknames[1]}",loc='right',fontsize=8))
-
-                                axs[r,c].spines['geo'].set_linewidth(1.5) #cartopy's recommended method
-                                axs[r,c].coastlines()
-                                axs[r,c].set_xticks(np.linspace(-180, 120, 6), crs=proj)
-                                axs[r,c].set_yticks(np.linspace(-90, 90, 7), crs=proj)
-                                axs[r,c].tick_params('both', length=5, width=1.5, which='major')
-                                axs[r,c].tick_params('both', length=5, width=1.5, which='minor')
-                                axs[r,c].xaxis.set_major_formatter(lon_formatter)
-                                axs[r,c].yaxis.set_major_formatter(lat_formatter)
-
-                            else:
-                                #Clear left over subplots if they don't fill the row x column matrix
-                                axs[r,c].set_visible(False)
-                            count = count + 1
-
-                    # __COLORBARS__
-                    fig.colorbar(img[-1], ax=axs.ravel().tolist(), orientation='horizontal',
-                                 aspect=20, shrink=.5, location="bottom",
-                                 anchor=(0.5,-0.3), extend='both')
+                        #Set figure title
+                        plt.suptitle(f'All Case Comparison (Test - Baseline)  {var}: {season}\n', fontsize=16, y=y_title)#y=0.325 y=0.225
                         
-                    #Clean up the spacing a bit
-                    plt.subplots_adjust(wspace=0.3, hspace=hspace)
-    
-                    fig.savefig(wks / file_name, bbox_inches='tight', dpi=300)
+                        normfunc,_ = use_this_norm()
 
-                    adfobj.add_website_data(wks / file_name, file_name, case_names[0], plot_ext="global_latlon_map",
-                                                        category=web_category, season=season, plot_type="LatLon",multi_case=True)
+                        count = 0
+                        img = []
+                        titles = []
+                        for r in range(0,nrows):
+                            for c in range(0,ncols):
+                                if count < nplots:
+                                    mdlfld = multi_dict[var][case_names[count]][season]["diff_data"]
+                                    lat = mdlfld['lat']
+                                    mwrap, lon = add_cyclic_point(mdlfld, coord=mdlfld['lon'])
 
-                    #Close plots:
-                    plt.close()
+                                    # mesh for plots:
+                                    lons, lats = np.meshgrid(lon, lat)
+
+                                    levelsdiff = multi_dict[var][case_names[count]][season]["vres"]["diff_contour_range"]
+                                    levelsdiff = np.arange(levelsdiff[0],levelsdiff[1]+levelsdiff[-1],levelsdiff[-1])
+
+                                    # color normalization for difference
+                                    if (np.min(levelsdiff) < 0) and (0 < np.max(levelsdiff)):
+                                        normdiff = normfunc(vmin=np.min(levelsdiff), vmax=np.max(levelsdiff), vcenter=0.0)
+                                    else:
+                                        normdiff = mpl.colors.Normalize(vmin=np.min(levelsdiff), vmax=np.max(levelsdiff))
+
+                                    cmap = multi_dict[var][case_names[count]][season]["vres"]['diff_colormap']
+
+                                    img.append(axs[r,c].contourf(lons, lats, mwrap, levels=levelsdiff, 
+                                                    cmap=cmap, norm=normdiff, 
+                                                    transform=proj))
+
+                                    #Set individual plot titles (case name/nickname)
+                                    titles.append(axs[r,c].set_title("$\mathbf{Test}:$"+f" {nicknames[0][count]}",loc='left',fontsize=8))
+                                    titles.append(axs[r,c].set_title("$\mathbf{Baseline}:$"+f" {nicknames[1]}",loc='right',fontsize=8))
+
+                                    axs[r,c].spines['geo'].set_linewidth(1.5) #cartopy's recommended method
+                                    axs[r,c].coastlines()
+                                    axs[r,c].set_xticks(np.linspace(-180, 120, 6), crs=proj)
+                                    axs[r,c].set_yticks(np.linspace(-90, 90, 7), crs=proj)
+                                    axs[r,c].tick_params('both', length=5, width=1.5, which='major')
+                                    axs[r,c].tick_params('both', length=5, width=1.5, which='minor')
+                                    axs[r,c].xaxis.set_major_formatter(lon_formatter)
+                                    axs[r,c].yaxis.set_major_formatter(lat_formatter)
+
+                                else:
+                                    #Clear left over subplots if they don't fill the row x column matrix
+                                    axs[r,c].set_visible(False)
+                                count = count + 1
+
+                        # __COLORBARS__
+                        fig.colorbar(img[-1], ax=axs.ravel().tolist(), orientation='horizontal',
+                                    aspect=20, shrink=.5, location="bottom",
+                                    anchor=(0.5,-0.3), extend='both')
+                            
+                        #Clean up the spacing a bit
+                        plt.subplots_adjust(wspace=0.3, hspace=hspace)
+        
+                        fig.savefig(wks / file_name, bbox_inches='tight', dpi=300)
+
+                        adfobj.add_website_data(wks / file_name, file_name, case_names[0], plot_ext="global_latlon_map",
+                                                            category=web_category, season=season, plot_type="LatLon",multi_case=True)
+
+                        #Close plots:
+                        plt.close()
 
 
 #####################
