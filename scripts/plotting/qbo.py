ab2ef7059583e1fb994b2b5e2bcaadbbe8666e6a
justin-richling <richling@ucar.edu>
2023-03-18 11:49:40 -0600

    Update qbo.py

--- scripts/plotting/qbo.py
+++ scripts/plotting/qbo.py
@@ -146,8 +146,8 @@ def qbo(adfobj):
                     fig_m = plt.figure(figsize=(16,16))
                     fig_m.suptitle('QBO Time Series', fontsize=14)
 
-                    #Plot ERA5 data
-                    ax_m = plotqbotimeseries(fig_m, obs, minny, x1[0], x2[0], y1[0], y2[0],'ERA5')
+                    """#Plot ERA5 data
+                    ax_m = plotqbotimeseries(fig_m, obs, minny, x1[0], x2[0], y1[0], y2[0],'ERA5')"""
                     
                     """#Plot individual case
                     ax_m = plotqbotimeseries(fig_m, casedat_5S_5N[icase],minny,
@@ -156,6 +156,9 @@ def qbo(adfobj):
 
                     #Check to plot baseline if not compared to obs
                     if not adfobj.compare_obs:
+                        #Plot ERA5 data
+                        ax_m = plotqbotimeseries(fig_m, obs, minny, x1[0], x2[0], y1[0], y2[0],'ERA5')
+
                         #Plot individual case
                         ax_m = plotqbotimeseries(fig_m, casedat_5S_5N[icase],minny,
                                                 x1[1],x2[1],y1[1],y2[1], 
@@ -164,9 +167,12 @@ def qbo(adfobj):
                                                     x1[2],x2[2],y1[2],y2[2], 
                                                     base_nickname)
                     else:
+                        #Plot ERA5 data
+                        ax_m = plotqbotimeseries(fig_m, obs, minny, 0.18, 0.45, y1[0], y2[0],'ERA5')
+
                         #Plot individual case
                         ax_m = plotqbotimeseries(fig_m, casedat_5S_5N[icase],minny,
-                                                x1[2],x2[2],y1[2],y2[2], 
+                                                0.55, 0.82, y1[2], y2[2], 
                                                 case_nicknames[icase])
 
                     ax_m = plotcolorbar(fig_m, x1[0]+0.2, x2[2]-0.2,y1[2]-0.035,y1[2]-0.03)
