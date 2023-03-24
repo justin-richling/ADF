dd41e83ced3717fc6527822ad5d56c678706420e
justin-richling <richling@ucar.edu>
2023-03-24 09:59:53 -0600

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -123,9 +123,17 @@ class AdfInfo(AdfConfig):
         case_names = self.get_cam_info('cam_case_name', required=True)
 
         #Grab test case nickname(s)
-        test_nicknames = self.get_cam_info('case_nickname')
+        test_nicknames = self.get_cam_info('case_nickname', required=True)
         if test_nicknames is None:
-            test_nicknames = [None]*len(case_names)
+            #test_nicknames = [None]*len(case_names)
+            for idx,case_name in enumerate(case_names):
+                test_nicknames[idx] = case_name
+
+        #Case names:
+        #case_names = self.get_cam_info('cam_case_name', required=True)
+        #for idx,nick_name in enumerate(test_nicknames):
+        #    if nick_name == None:
+        #        test_nicknames[idx] = case_names[idx]
 
         #Check if a CAM vs AMWG obs comparison is being performed:
         if self.__compare_obs:
@@ -175,8 +183,12 @@ class AdfInfo(AdfConfig):
                 #End if
             #End if
 
+            #Grab baseline nickname
+            base_nickname = self.get_baseline_info('case_nickname')
+            if base_nickname == None:
+                base_nickname = data_name
+
             data_name += f"_{syear_baseline}_{eyear_baseline}"
-            base_nickname = self.get_baseline_info('case_nickname')        
         #End if
 
         #Initialize case nicknames:
@@ -405,18 +417,18 @@ class AdfInfo(AdfConfig):
         #Grab test case nickname(s)
         test_nicknames = copy.copy(self.__test_nicknames)
 
-        #Case names:
+        """#Case names:
         case_names = self.get_cam_info('cam_case_name', required=True)
         for idx,nick_name in enumerate(test_nicknames):
             if nick_name == None:
-                test_nicknames[idx] = case_names[idx]
+                test_nicknames[idx] = case_names[idx]"""
 
         #Grab test case nickname(s)
         base_nickname = copy.copy(self.__base_nickname)
 
-        #Grab baseline nickname
+        """#Grab baseline nickname
         if base_nickname == None:
-            base_nickname = self.get_baseline_info('cam_case_name', required=True)
+            base_nickname = self.get_baseline_info('cam_case_name', required=True)"""
 
         return {"test_nicknames":test_nicknames,"base_nickname":base_nickname}
 
