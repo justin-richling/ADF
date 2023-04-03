a4a17bef9fcb4157419dfcec701a8410f1ee19da
justin-richling <richling@ucar.edu>
2023-04-03 08:59:45 -0600

    Update amwg_table.py

--- scripts/analysis/amwg_table.py
+++ scripts/analysis/amwg_table.py
@@ -447,7 +447,7 @@ def _df_comp_table(adf, output_location, case_names):
     df_comp.to_csv(output_csv_file_comp, header=cols_comp, index=False)
 
     #Add comparison table dataframe to website (if enabled):
-    adf.add_website_data(df_comp, "Case Comparison", case_names[0], plot_type="Tables")
+    adf.add_website_data(df_comp, "case_comparison", case_names[0], plot_type="Tables")
 
 #####
 
