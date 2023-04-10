8445de7a35f7b6c78a88b07637284fc19ba682b8
justin-richling <richling@ucar.edu>
2023-04-10 10:21:23 -0600

    Update amwg_table.py

--- scripts/analysis/amwg_table.py
+++ scripts/analysis/amwg_table.py
@@ -177,7 +177,7 @@ def amwg_table(adf):
         derived_dict = {}
     
     else:
-        derived_dict = []
+        derived_var_list = []
 
     #Hold output paths for csv files
     csv_locs = []
