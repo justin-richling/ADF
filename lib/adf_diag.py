d65f5eb3b88f9ae42a93002b9d3b5aa0d9b6dc45
justin-richling <richling@ucar.edu>
2023-04-12 14:17:29 -0600

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -742,6 +742,10 @@ class AdfDiag(AdfWeb):
 
 
         print("TRY this:",plot_func_names,"\n")
+
+        for i in plot_func_names:
+            if type(i) == "dictionary":
+                print("Getting there\n")
         
         #If no scripts are listed, then exit routine:
         if not plot_func_names:
