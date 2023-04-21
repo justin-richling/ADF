c3e4b5a1c8feede1c512990529713c8a6b92ae5a
justin-richling <richling@ucar.edu>
2023-04-21 12:18:03 -0600

    Update amwg_chem_table.py

--- scripts/analysis/amwg_chem_table.py
+++ scripts/analysis/amwg_chem_table.py
@@ -146,6 +146,7 @@ def amwg_chem_table(adf):
     # Look for specific h-case    
     #scenarios = [f'{ix}.cam.{h_case}' for ix in case_names]
     scenarios = [f'{ix}.{h_case}' for ix in case_names]
+    print(scenarios)
 
 
     # In CAM-Chem (or MUSICA-v0), user can save the outputs for only a box region.
