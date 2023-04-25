158908d4bf8037bbff9d6e5f34f2ac825d848e20
justin-richling <richling@ucar.edu>
2023-04-25 10:32:10 -0600

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -486,7 +486,7 @@ class AdfWeb(AdfObs):
                         base_dir_ext = f"{data_name}"
                     else:
                         base_dir_ext = f"{data_name}_{syear_baseline}_{eyear_baseline}"
-                    case_sites[case_name] = [os.path.join(os.curdir,
+                    case_sites[case_name] = [os.path.join("../",
                                              f"{case_dir_ext}_vs_{base_dir_ext}",
                                              "index.html"),syear_cases[idx],eyear_cases[idx]]
 
