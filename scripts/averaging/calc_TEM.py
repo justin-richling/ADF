303262c2726cbd7fe2ecf094fe502370f81c6353
justin-richling <richling@ucar.edu>
2023-05-01 09:06:41 -0600

    Update calc_TEM.py

--- scripts/averaging/calc_TEM.py
+++ scripts/averaging/calc_TEM.py
@@ -80,11 +80,11 @@ def calc_TEM(adf):
         print("ds.time.values",ds.time.values,"\n")
 
         #iterate over the times in a dataset
-        for idx, ds_date in enumerate(ds.time.values):
+        for idx,_ in enumerate(ds.time.values):
             if idx == 0:
-                dstem0 = calc_tem(ds.squeeze().isel(time=ds_date))
+                dstem0 = calc_tem(ds.squeeze().isel(time=idx))
             else:
-                dstem = calc_tem(ds.squeeze().isel(time=ds_date))
+                dstem = calc_tem(ds.squeeze().isel(time=idx))
                 dstem0 = xr.concat([dstem0, dstem],'time')
             #End if
         #End if    
