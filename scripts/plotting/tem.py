8457081d091748ed98559cd242a54ce8fbad9b4c
justin-richling <richling@ucar.edu>
2023-05-05 09:10:09 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -291,7 +291,7 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res):
 
             oseasons.plot(ax=axs[0,1], y='lev', yscale='log',ylim=[1e3,1],
                                     cbar_kwargs={'label': ds_base[var].units})
-            axs[0,1].set_title(f"{case_names[1]}\n\n{ds_base[var].long_name}\n",fontsize=14)
+            axs[0,1].set_title(f"{case_names[1]}\n\n"+"$\mathbf{"+ds_base[var].long_name+"}$"+"\n",fontsize=14)
 
             #dseasons = mseasons-oseasons
             dseasons.plot(ax=axs[0,2], y='lev', yscale='log', ylim=[1e3,1],cmap="BrBG",
