502e8e9e6ccaf10f08a86627d7ccf91018f635e6
justin-richling <richling@ucar.edu>
2023-05-05 12:26:20 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -437,7 +437,7 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res):
     axs[0,2].set_title("$\mathbf{Test} - \mathbf{Baseline}$"+"\n\n\n",fontsize=14)
     #Set variable name on center plot
     for i,var_name in enumerate(var_list):
-        axs[i,1].set_title("$\mathbf{"+var_name+"}$"+"\n",fontsize=14)
+        axs[i+1,1].set_title("$\mathbf{"+var_name+"}$"+"\n",fontsize=14)
     
     #Adjust subplots
     hspace = 0.4
