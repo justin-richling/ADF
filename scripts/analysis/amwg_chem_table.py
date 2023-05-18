7cf9f939e8aa0dd65fe66929cfaabd666ab40db0
justin-richling <richling@ucar.edu>
2023-05-18 15:26:05 -0600

    Update amwg_chem_table.py

--- scripts/analysis/amwg_chem_table.py
+++ scripts/analysis/amwg_chem_table.py
@@ -823,6 +823,7 @@ def SEbudget(dic_SE,data_dir,files,var,**kwargs):
             #print(i,"\n")
             #Check to see if the product is in the actual dataset, if not, move on and set to 0
             if i in ds:
+                print(f"Looks like {var} is {i} for {file}, so good to go...\n")
                 data.append(np.array(ds[i].isel(time=0))*dic_SE[var][i])
             else:
                 print(f"Looks like {var} is missing {i} for {file}, so skipping...\n")
