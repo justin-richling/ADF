5dd96d8f89f178c76daf61d978797ccda952c1b3
justin-richling <richling@ucar.edu>
2023-06-01 10:17:15 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -548,8 +548,8 @@ def tem_plot(adf, ds, ds_base, case_names, axs, s, var_list, res):
     #Set titles of subplots
     #Set case names in first subplot only
     uzm = ds["uzm"].long_name.replace(" ", "\ ")
-    axs[0,0].set_title(f"\n"+"$\mathbf{Test}$\n"+f"{case_names[0]}\n\n\n",fontsize=14)
-    axs[0,1].set_title(f"\n"+"$\mathbf{Baseline}$\n"+f"{case_names[1]}\n\n"+"$\mathbf{"+uzm+"}$"+"\n",fontsize=14)
+    axs[0,0].set_title(f"\n\n"+"$\mathbf{Test}$\n"+f"{case_names[0]}\n\n\n",fontsize=14)
+    axs[0,1].set_title(f"\n\n"+"$\mathbf{Baseline}$\n"+f"{case_names[1]}\n\n"+"$\mathbf{"+uzm+"}$"+"\n",fontsize=14)
     axs[0,2].set_title("$\mathbf{Test} - \mathbf{Baseline}$"+"\n\n\n",fontsize=14)
     #Set variable name on center plot
     for i in range(1,len(var_list)):
