e7fce34cf7d520bee15a7304aa1f15ae9fac8bab
justin-richling <richling@ucar.edu>
2023-07-22 14:43:06 -0600

    Update zonal_mean.py

--- scripts/plotting/zonal_mean.py
+++ scripts/plotting/zonal_mean.py
@@ -232,9 +232,6 @@ def zonal_mean(adfobj):
                 # Check redo_plot. If set to True: remove old plot, if it already exists:
                 if (not redo_plot) and plot_name.is_file():
                     zonal_skip.append(plot_name)
-                    #Add already-existing plot to website (if enabled):
-                    adfobj.add_website_data(plot_name, var, case_name, season=s,
-                                                        plot_type="Zonal")
 
                     continue
                 elif (redo_plot) and plot_name.is_file():
