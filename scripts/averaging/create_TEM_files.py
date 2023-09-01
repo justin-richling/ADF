d9584a4844dd4df39c568ccf4bb54467757dc1ba
justin-richling <richling@ucar.edu>
2023-09-01 13:00:51 -0600

    Update create_TEM_files.py

--- scripts/averaging/create_TEM_files.py
+++ scripts/averaging/create_TEM_files.py
@@ -110,7 +110,8 @@ def create_TEM_files(adf):
             print(f"'{tem_fil}' already exists and wont be over-written, moving on")
             pass
         else:
-            print("I take it this we never made it here, eh?")
+            if tem_fil.is_file():
+                print(f"'{tem_fil}' already exists but will be over-written")
             #Group all TEM observation files together
             tem_obs_fils = []
             for var in var_list:
@@ -205,6 +206,8 @@ def create_TEM_files(adf):
             print(f"'{tem_fil}' already exists and wont be over-written, moving on")
             pass
         else:
+            if tem_fil.is_file():
+                print(f"'{tem_fil}' already exists but will be over-written")
             #Glob each set of years
             #NOTE: This will make a nested list
             hist_files = []
