57f65db33dc68e4bfb3af34b5af3fc6dd0530a0a
justin-richling <richling@ucar.edu>
2023-10-06 08:33:56 -0600

    Update create_climo_files.py

--- scripts/averaging/create_climo_files.py
+++ scripts/averaging/create_climo_files.py
@@ -215,6 +215,8 @@ def process_variable(ts_files, syr, eyr, output_file):
     enc_c  = {xname: {'_FillValue': None} for xname in cam_climo_data.coords}
     enc    = {**enc_c, **enc_dv}
 
+    cam_climo_data = cam_climo_data.assign_attrs(climo_yrs=f"{syr}-{eyr}")
+
     #Output variable climatology to NetCDF-4 file:
     cam_climo_data.to_netcdf(output_file, format='NETCDF4', encoding=enc)
     return 1  # All funcs return something. Could do error checking with this if needed.
