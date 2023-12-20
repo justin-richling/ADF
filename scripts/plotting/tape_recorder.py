5f809ddf374e98447b3ddc6cd82ea3561cf8735f
justin-richling <richling@ucar.edu>
2023-12-20 12:07:29 -0700

    Update tape_recorder.py

--- scripts/plotting/tape_recorder.py
+++ scripts/plotting/tape_recorder.py
@@ -266,7 +266,8 @@ def tape_recorder(adfobj):
             fig.savefig(plot_name, bbox_inches='tight', facecolor='white')
 
             #Add plot to website (if enabled):
-            adfobj.add_website_data(plot_name, "tape_recorder", None, season="ANN", multi_case=True)
+            adfobj.add_website_data(plot_name, "tape_recorder", case_names[idx], season="ANN", multi_case=True, plot_type="Special")
+            #adfobj.add_website_data(plot_name, "tape_recorder", None, season="ANN", multi_case=True)
         
 
     runname_LT=xr.DataArray(runname_LT, dims='run', coords=[np.arange(0,len(runname_LT),1)], name='run')
