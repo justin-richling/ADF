38fe716d8734aa445e6a934ce0b2bd8079d8d6c5
justin-richling <richling@ucar.edu>
2023-12-20 12:46:18 -0700

    Update tape_recorder.py

--- scripts/plotting/tape_recorder.py
+++ scripts/plotting/tape_recorder.py
@@ -251,7 +251,7 @@ def tape_recorder(adfobj):
             #    x2_loc = x2[3]
             
             x1_loc = (x1[1]-x1[0])/2
-            x2_loc = ((x2[2]-x2[1])/2)+x2[1]
+            x2_loc = ((x2[3]-x2[2])/2)+x2[2]
 
             y1_loc = y1[count]-0.03
             y2_loc = y1[count]-0.02
