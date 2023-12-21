d8aef66a27bc66fbc007fec0af91dbc08058c17b
justin-richling <richling@ucar.edu>
2023-12-21 11:43:57 -0700

    Update global_latlon_map.py

--- scripts/plotting/global_latlon_map.py
+++ scripts/plotting/global_latlon_map.py
@@ -462,7 +462,8 @@ def global_latlon_map(adfobj):
                                 if multi_plots:
                                     #multi_dict[f"{var}_{pres}"] = {}
                                     #multi_dict[f"{var}_{pres}"][case_name] = {}
-                                    multi_dict[f"{var}_{pres}"][case_name][s] = {}
+                                    if s not in multi_dict[f"{var}_{pres}"][case_name]:
+                                        multi_dict[f"{var}_{pres}"][case_name][s] = {}
                                     if var in adfobj.get_multi_case_info("global_latlon_map"):
                                         multi_dict[f"{var}_{pres}"][case_name][s] = {"diff_data":dseasons[s],"vres":vres}
 
