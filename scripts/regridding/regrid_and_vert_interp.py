c7cec1fa90863561c6a564f78b70944e5a67addd
justin-richling <richling@ucar.edu>
2023-12-22 16:48:21 -0700

    Update regrid_and_vert_interp.py

--- scripts/regridding/regrid_and_vert_interp.py
+++ scripts/regridding/regrid_and_vert_interp.py
@@ -166,8 +166,12 @@ def regrid_and_vert_interp(adf):
         #attr_dict = {f"{case_name}": f"{syear}-{eyear}",
         #             f"{target_list[0]}": f"{syear_baseline}-{eyear_baseline}"}
 
-        attr_dict = {"climo_yrs": [f"{case_name}: {syear}-{eyear}", f"\n{target_list[0]}: {syear_baseline}-{eyear_baseline}"]
-                    }
+        #attr_dict = {"climo_yrs": [f"{case_name}: {syear}-{eyear}",
+        #                           f"{target_list[0]}: {syear_baseline}-{eyear_baseline}"]
+        #            }
+
+        attr_dict = {"test_climo_yrs": f"{case_name}: {syear}-{eyear}",
+                     "baseline_climo_yrs": f"{target_list[0]}: {syear_baseline}-{eyear_baseline}"}
 
         #Update attrs dict for current test case climo years
         #attr_dict["test_climo_yrs"] = f"{syear}-{eyear}"
