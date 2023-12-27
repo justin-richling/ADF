2132adaad8fbdd9dd2291316e9f924669e587f52
justin-richling <richling@ucar.edu>
2023-12-27 14:45:01 -0700

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -1034,6 +1034,9 @@ class AdfDiag(AdfWeb):
             #cmd2 = f"ncap2 -s '{var}=({der_eq})' {constit_files[1]} {derived_file}"
             cmd2 = f"ncap2 -s '{var}=({der_eq})' {constit_files[1]}"
             cmd_p2 = cmd2
+
+            print(constit_files,"\n")
+
             for i2 in constit_files[1:]:
                 cmd_p2 = f"{cmd_p2} {i2} "
             cmd_p2 = cmd_p2 + f"{derived_file}"
