388be964afa0a45fbae5ba1d70154674d4d87bcf
justin-richling <richling@ucar.edu>
2024-02-12 13:07:55 -0700

    Update vert_seasonal_cycle.py

--- scripts/plotting/vert_seasonal_cycle.py
+++ scripts/plotting/vert_seasonal_cycle.py
@@ -148,9 +148,9 @@ def vert_seasonal_cycle(adfobj):
         eyr = eyear_cases[idx]
 
         #make_zm_files(hist_loc,case_name,calc_var_list,syr,eyr,return_ds=True):
-        #ncfile = make_zm_files(adfobj,hist_loc,case_name,calc_var_list,syr,eyr,return_ds=True)
-        file = "/glade/work/richling/ADF/ADF_dev/notebooks/chem-diags/new_tests/waccm_135_acom_ne16pg3_ne16pg3_mg17_1536_long2.nc"
-        ncfile = xr.open_dataset(file, decode_times=True, use_cftime=True)
+        ncfile = make_zm_files(adfobj,hist_loc,case_name,calc_var_list,syr,eyr,return_ds=True)
+        #file = "/glade/work/richling/ADF/ADF_dev/notebooks/chem-diags/new_tests/waccm_135_acom_ne16pg3_ne16pg3_mg17_1536_long2.nc"
+        #ncfile = xr.open_dataset(file, decode_times=True, use_cftime=True)
 
         #zmdir = run
         #file = f"waccm_135_{zmdir}.nc"
