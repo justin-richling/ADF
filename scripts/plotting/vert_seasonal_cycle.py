49396329642eb1d0fb2ceaf8e0cf6ac76bb9b612
justin-richling <richling@ucar.edu>
2024-02-12 13:45:08 -0700

    Update vert_seasonal_cycle.py

--- scripts/plotting/vert_seasonal_cycle.py
+++ scripts/plotting/vert_seasonal_cycle.py
@@ -461,7 +461,7 @@ def comparison_plots(adfobj, cam_var, case_names, case_ds_dict, obs_ds_dict, tim
     #print("plot_locations",plot_locations)
     plot_loc = Path(plot_locations[0])
     plot_type = "png"
-    plot_name = plot_loc / f"{cam_var}_{str_interval}_Zonal_Mean_scycle.{plot_type}"
+    plot_name = plot_loc / f"{cam_var}_{str_interval}_Zonal_scycle_Mean.{plot_type}"
     if time_avg == "month":
         adfobj.add_website_data(plot_name, cam_var, case_name, season=str_interval, plot_type="Zonal", category="SeasonalCycleMonth",non_season=True)
     else:
