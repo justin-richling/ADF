2e9b314611cf3cd6d5893fe3eb55c48b5897de3a
justin-richling <richling@ucar.edu>
2024-02-28 14:42:55 -0700

    as

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -350,13 +350,13 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res, obs, climo_yrs):
             mseasons.plot(ax=axs[3,0], y='lev', yscale='log',vmax=1e5,ylim=[1e2,1],
                                     cbar_kwargs={'label': ds[var].units})
             mseasons.plot.contour(ax=axs[3,0],  y='lev', yscale='log',
-                                                ylim=[1e2,1],
+                                                levels = 21,ylim=[1e2,1],
                                                 colors='black', linestyles=None)
 
             oseasons.plot(ax=axs[3,1], y='lev', yscale='log',vmax=1e5,ylim=[1e2,1],
                                     cbar_kwargs={'label': ds[var].units})
             oseasons.plot.contour(ax=axs[3,1],  y='lev', yscale='log',
-                                                ylim=[1e2,1],
+                                                levels = 21,ylim=[1e2,1],
                                                 colors='black', linestyles=None)
 
             #Check if difference plot has contour levels, if not print notification
@@ -370,17 +370,17 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res, obs, climo_yrs):
         # TEM meridional wind 
         #------------------------------------------------------------------------------------------
         if var == "vtem":
-            mseasons.plot.contourf(ax=axs[4,0], y='lev', yscale='log',
+            mseasons.plot.contourf(ax=axs[4,0], levels = 21,y='lev', yscale='log',
                                                 vmax=3,vmin=-3,ylim=[1e2,1], cmap='RdBu_r',
                                                 cbar_kwargs={'label': ds[var].units})
-            mseasons.plot.contour(ax=axs[4,0],  y='lev', yscale='log',
+            mseasons.plot.contour(ax=axs[4,0],  y='lev', yscale='log',levels = 21,
                                                 vmax=3,vmin=-3,ylim=[1e2,1],
                                                 colors='black', linestyles=None)
 
-            oseasons.plot.contourf(ax=axs[4,1],  y='lev', yscale='log',
+            oseasons.plot.contourf(ax=axs[4,1], levels = 21, y='lev', yscale='log',
                                                 vmax=3,vmin=-3,ylim=[1e2,1], cmap='RdBu_r',
                                                 cbar_kwargs={'label': ds[var].units})
-            oseasons.plot.contour(ax=axs[4,1],  y='lev', yscale='log',
+            oseasons.plot.contour(ax=axs[4,1],  y='lev', yscale='log',levels = 21,
                                                 vmax=3,vmin=-3,ylim=[1e2,1],
                                                 colors='black', linestyles=None)
 
