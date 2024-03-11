2bb3be2ba55281871f3e1d01e8618db4aead933d
justin-richling <richling@ucar.edu>
2024-03-11 11:58:14 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -365,7 +365,7 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res, obs, climo_yrs):
                 axs[2,2].text(prop_x, prop_y, empty_message, transform=axs[3,2].transAxes, bbox=props)
             else:
                 dseasons.plot(ax=axs[3,2], y='lev', yscale='log',
-                            ylim=[1e2,1],cmap="BrBG",levels=np.arange(-0.1e7,0.1e7,0.05e7)
+                            ylim=[1e2,1],cmap="BrBG",levels=np.arange(-0.1e7,0.1e7,0.05e7),
                                     cbar_kwargs={'label': ds[var].units})
 
         # TEM meridional wind 
