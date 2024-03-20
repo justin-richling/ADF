06ebea7c447f008eae656fefd53f9715443c82c0
justin-richling <richling@ucar.edu>
2024-03-20 13:56:36 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -513,6 +513,7 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res, obs, climo_yrs):
             def log_formatter(x, pos):
                 """Custom formatter for logarithmic scale"""
                 if x < 0:
+                    print(f"{x:.2f}")
                     return f"{x:.2f}"
                 else:
                     return f"{int(x)}"
