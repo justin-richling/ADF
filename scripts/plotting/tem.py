09896deef33ed94dee085cbea0bf010d09805e28
justin-richling <richling@ucar.edu>
2024-03-20 11:58:11 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -318,7 +318,8 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res, obs, climo_yrs):
             levels1 = vres['contour_levels']
             if ('non_linear' in vres) and (vres['non_linear']):
                 cmap_obj = cm.get_cmap(cmap1)
-                norm1 = mpl.colors.BoundaryNorm(levels1, cmap_obj.N)
+                #norm1 = mpl.colors.BoundaryNorm(levels1, cmap_obj.N)
+                norm1 = mpl.colors.Normalize(vmin=min(levels1), vmax=max(levels1))
             else:
                 norm1 = mpl.colors.Normalize(vmin=min(levels1), vmax=max(levels1))
         elif 'contour_levels_range' in vres:
@@ -329,6 +330,7 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res, obs, climo_yrs):
             lev_range = [float(x) for x in vres['contour_levels_range']]
 
             levels1 = np.arange(*lev_range)
+            
             if ('non_linear' in vres) and (vres['non_linear']):
                 cmap_obj = cm.get_cmap(cmap1)
                 #norm1 = mpl.colors.BoundaryNorm(levels1, cmap_obj.N)
