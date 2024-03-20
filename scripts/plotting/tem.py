53283e8516e22301e3ed2337c88bec7cdfeb165e
justin-richling <richling@ucar.edu>
2024-03-20 11:00:34 -0600

    asd

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -368,7 +368,6 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res, obs, climo_yrs):
         # Zonal mean temperature
         #------------------------------------------------------------------------------------------
         if var == "thzm":
-            continue
             #mseasons = np.log(mseasons)
             #oseasons = np.log(oseasons)
 
