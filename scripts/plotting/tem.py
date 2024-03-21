ed0cc1ebd969b11639f4520f0ea1f85697a7e687
justin-richling <richling@ucar.edu>
2024-03-21 09:24:14 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -400,7 +400,7 @@ def tem_plot(ds, ds_base, case_names, axs, s, var_list, res, obs, climo_yrs):
         else:
             img2 = axs[axs_id,2].contourf(lats,levs,dseasons, cmap="BrBG",levels=levs_diff)
             axs[axs_id,2].contour(lats,levs,dseasons, colors="k",)#levels=diff_levs[::2]
-            plt.colorbar(img2, ax=axs[axs_id,2], location='right',**cp_info['diff_colorbar_opt'])
+            plt.colorbar(img2, ax=axs[axs_id,2], location='right',)#**cp_info['diff_colorbar_opt']
 
         #Format y-axis
         for a in axs[axs_id,:]:
