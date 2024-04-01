4c4612e18c8d42e866c43c4d89b0891f8843bf16
justin-richling <richling@ucar.edu>
2024-04-01 10:56:46 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -3372,7 +3372,8 @@ def waccm_qbo(plot_name, case_names, nicknames, case_runs, merra2, syear_cases, 
         axes[main_key[idx]].set_xticks(np.arange(0,(nt_sub),12),rotation=40)
         #axes[main_key[idx]].set_xticklabels(np.arange(int(yrs+int(nt_sub/12)),int(yrs+int(nt_sub/12))+int(nt_sub/12)+1,1))
         yr0 = int(yrs+int(start_idx/12))
-        axes[main_key[idx]].set_xticklabels(np.arange(yr0, yr0+int(nt_sub/12)+1, 1), fontsize=8)
+        #axes[main_key[idx]].set_xticklabels(np.arange(yr0, yr0+int(nt_sub/12)+1, 1), fontsize=8)
+        axes[main_key[idx]].set_xticklabels(np.arange(yr0, yr0+int(nt_sub/12), 1), fontsize=8)
         #axes[main_key[merra_plot]].tick_params(axis='y', labelsize=10)
 
         #Case QBO Amplitude side axis
