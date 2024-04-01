a3d516c50d49a683e5288c5c810688208f68b56b
justin-richling <richling@ucar.edu>
2024-04-01 12:17:28 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -3335,7 +3335,7 @@ def waccm_qbo(plot_name, case_names, nicknames, case_runs, merra2, syear_cases, 
         #QUESTION: what if the data doesn't have 9 years? - we will need to clip this...
         start_idx = 0 #119-24
         #print(plotdata[start_idx:start_idx+(12*9),:].shape)
-        end_idx = start_idx+(12*9)+1
+        #end_idx = start_idx+(12*9)+1
         end_idx = start_idx+nt+1
         #print("AHH",start_idx+(12*9)+1,"\n")
         print("end_idx",end_idx,"\n\n")
@@ -3377,7 +3377,7 @@ def waccm_qbo(plot_name, case_names, nicknames, case_runs, merra2, syear_cases, 
         # Set the x-axis limits
         yr0 = int(yrs+int(start_idx/12))
         axes[main_key[idx]].set_xlim(yr0, yr0+10)  # Set the limits from 0 to 11
-        #axes[main_key[idx]].set_xticks(range(yr0, yr0+10, 1))
+        axes[main_key[idx]].set_xticks(range(yr0, yr0+11, 1))
         #print("nt_sub",nt_sub)
         #axes[main_key[idx]].set_xticks(np.arange(0,(nt_sub)+1,12),rotation=40)
         #axes[main_key[idx]].set_xticks(np.arange(0,(nt_sub),12),rotation=40)
