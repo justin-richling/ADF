c6e366b0384184fe9049d08d1980f585892c1b8a
justin-richling <richling@ucar.edu>
2024-04-01 12:26:08 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -3341,7 +3341,7 @@ def waccm_qbo(plot_name, case_names, nicknames, case_runs, merra2, syear_cases, 
         print("end_idx",end_idx,"\n\n")
 
 
-        print("WTF",time_grid[start_idx:end_idx-1,:],"\n")
+        #print("WTF",time_grid[start_idx:end_idx-1,:],"\n")
         yr0 = int(yrs+int(start_idx/12))
         #axes[main_key[idx]].set_xlim(yr0, yr0+10)  # Set the limits from 0 to 11
         #axes[main_key[idx]].set_xticks(range(yr0, yr0+11, 1))
@@ -3387,6 +3387,9 @@ def waccm_qbo(plot_name, case_names, nicknames, case_runs, merra2, syear_cases, 
         axes[main_key[idx]].set_xlim(yr0, yr0+10)  # Set the limits from 0 to 11
         axes[main_key[idx]].set_xticks(range(yr0, yr0+11, 1))
         """
+
+        axes[main_key[idx]].set_xticks(range(yr0, yr0+11, 1))
+
         #print("nt_sub",nt_sub)
         #axes[main_key[idx]].set_xticks(np.arange(0,(nt_sub)+1,12),rotation=40)
         #axes[main_key[idx]].set_xticks(np.arange(0,(nt_sub),12),rotation=40)
@@ -3402,7 +3405,7 @@ def waccm_qbo(plot_name, case_names, nicknames, case_runs, merra2, syear_cases, 
             need_yrs.append(yr0+i)
         alright = np.arange(yr0, yr0+int(nt_sub/12), 1)
         if alright[-1] < yr0+10:
-            print()
+            print("yfguhbijnkm")
             axes[main_key[idx]].set_xticklabels(np.arange(yr0, yr0+11, 1), fontsize=8)
         if alright[-1] == yr0+10:
             axes[main_key[idx]].set_xticklabels(np.arange(yr0, yr0+int(nt_sub/12), 1), fontsize=8)
