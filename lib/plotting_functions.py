f220e31b216797a74036e4639c57e147972a9614
justin-richling <richling@ucar.edu>
2024-04-01 12:39:18 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -3388,7 +3388,8 @@ def waccm_qbo(plot_name, case_names, nicknames, case_runs, merra2, syear_cases, 
         axes[main_key[idx]].set_xticks(range(yr0, yr0+11, 1))
         """
 
-        axes[main_key[idx]].set_xticks(range(0, 11, 1))
+        #axes[main_key[idx]].set_xticks(range(0, 11, 1))
+        axes[main_key[idx]].set_xlim(yr0, yr0+10)
 
         #print("nt_sub",nt_sub)
         #axes[main_key[idx]].set_xticks(np.arange(0,(nt_sub)+1,12),rotation=40)
@@ -3406,6 +3407,7 @@ def waccm_qbo(plot_name, case_names, nicknames, case_runs, merra2, syear_cases, 
         alright = np.arange(yr0, yr0+int(nt_sub/12), 1)
         if alright[-1] < yr0+10:
             print("yfguhbijnkm")
+            axes[main_key[idx]].set_xlim(yr0, yr0+10)  # Set the limits from 0 to 11
             axes[main_key[idx]].set_xticklabels(np.arange(yr0, yr0+11, 1), fontsize=8)
         if alright[-1] == yr0+10:
             axes[main_key[idx]].set_xticklabels(np.arange(yr0, yr0+int(nt_sub/12), 1), fontsize=8)
