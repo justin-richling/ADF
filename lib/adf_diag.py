d9a96babe1b607c99db7365c13c53f07983092fe
justin-richling <richling@ucar.edu>
2024-04-16 16:12:25 -0600

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -429,7 +429,7 @@ class AdfDiag(AdfWeb):
                                 if vres["derive"]["method"] == "interp":
                                     for dim in ["time","lat","lon","lev","ilev"]:
                                         if dim in vres["derive"].keys():
-                                            print(res["derive"]["method"][dim])
+                                            print(vres["derive"]["method"][dim])
                                             print(dim)
                                             der_from = vres['derive']['derivable_from']
                                             ts_exist = glob.glob(os.path.join(ts_case_dir, f"*.{der_from}.*"))
