2135fd8480cd712e4583b9e0612e1d46d66677ab
justin-richling <richling@ucar.edu>
2024-04-22 12:06:34 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1308,7 +1308,7 @@ def plot_map_and_save(wks, case_nickname, base_nickname,
                                         dateline_direction_label=False)
     lat_formatter = LatitudeFormatter(number_format='0.0f',
                                         degree_symbol='')
-
+    print("paleo??",paleo)
     if paleo:
         #Threshold land fraction values to identify land areas
         land_mask = landfrac_da > 0.5
@@ -1347,7 +1347,7 @@ def plot_map_and_save(wks, case_nickname, base_nickname,
                        transform=ccrs.PlateCarree(), transform_first=True,extend='both',
                        **cp_info['contourf_opt']))
             if paleo:
-                img.append(ax.contour(land_mask.lon, land_mask.lat, land_mask, levels=[0.5], colors='black'))
+                img.append(ax[i].contour(land_mask.lon, land_mask.lat, land_mask, levels=[0.5], colors='black'))
         """if i == 0:
             img.append(ax[i].contourf(lons, lats, a, levels=test_levels, cmap=cmap, norm=test_norm,
                        transform=ccrs.PlateCarree(), transform_first=True,
