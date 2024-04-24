387ad65f77f040a216ec758c85a323b049cf79b8
justin-richling <richling@ucar.edu>
2024-04-24 14:56:55 -0600

    Update tape_recorder.py

--- scripts/plotting/tape_recorder.py
+++ scripts/plotting/tape_recorder.py
@@ -169,6 +169,7 @@ def tape_recorder(adfobj):
     runname_LT=[]
     for idx,key in enumerate(runs_LT2):
         fils= sorted(Path(runs_LT2[key]).glob(f'*{adfobj.hist_str}*.Q.*.nc'))
+        print(fils,"\n")
         dat = pf.load_dataset(str(fils))
 
         #Check if data files exist, skip current case if not
