6cf72082d47d5edd65e8940da5e80a5514ae8378
justin-richling <richling@ucar.edu>
2024-04-26 11:30:51 -0600

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -181,7 +181,7 @@ class AdfInfo(AdfConfig):
             skip_baseline_ts = False
 
             if not calc_baseline_ts:
-                print("\nWARNING: User indicates they don't want to rely on timeseries or history files.")
+                print(f"\nWARNING: User indicates they don't want to rely on timeseries or history files for '{data_name}'.")
                 print("  - The climo years specified in the config file cannot be verified!")
                 skip_baseline_ts = True
 
@@ -470,7 +470,7 @@ class AdfInfo(AdfConfig):
             eyear = eyears[case_idx]
 
             if not calc_case_ts[case_idx]:
-                print("\nWARNING: User indicates they don't want to rely on timeseries or history files.")
+                print(f"\nWARNING: User indicates they don't want to rely on timeseries or history files for '{case_name}.")
                 print("  - The climo years specified in the config file cannot be verified!")
                 skip_cam_ts[case_idx] = True
                 #continue
