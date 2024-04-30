a14c81582d026167b0e1d490dd09158ef0da728a
justin-richling <richling@ucar.edu>
2024-04-30 10:33:25 -0600

    Update create_climo_files.py

--- scripts/averaging/create_climo_files.py
+++ scripts/averaging/create_climo_files.py
@@ -82,9 +82,11 @@ def create_climo_files(adf, clobber=False, search=None):
     if not adf.get_basic_info("compare_obs"):
         #Extract CAM baseline variaables:
         baseline_name     = adf.get_baseline_info("cam_case_name", required=True)
-        input_ts_baseline = adf.get_baseline_info("cam_ts_loc", required=True)
-        output_bl_loc     = adf.get_baseline_info("cam_climo_loc", required=True)
         calc_bl_climos    = adf.get_baseline_info("calc_cam_climo")
+        if calc_bl_climos:
+            cam_ts_loc_req = True
+            input_ts_baseline = adf.get_baseline_info("cam_ts_loc", required=True)
+        output_bl_loc     = adf.get_baseline_info("cam_climo_loc", required=True)
         ovr_bl            = adf.get_baseline_info("cam_overwrite_climo")
 
         #Extract baseline years:
