13194a4e1f6739dc7057d6e42127b44d614f20fe
justin-richling <richling@ucar.edu>
2024-06-14 15:02:13 -0600

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -569,6 +569,7 @@ class AdfDiag(AdfWeb):
                     elif constit_list is None:
                         print("is it none???")
                         #diag_var_list.remove(var)
+                        continue
                     else:
                         constit_dict[var] = constit_list
                         continue
