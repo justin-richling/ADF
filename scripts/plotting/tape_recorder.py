fc0468a7143f61732c341174f31c0326d9625174
justin-richling <richling@ucar.edu>
2024-06-24 16:32:32 -0600

    Update tape_recorder.py

--- scripts/plotting/tape_recorder.py
+++ scripts/plotting/tape_recorder.py
@@ -41,7 +41,8 @@ def tape_recorder(adfobj):
 
     cam_hist_strs = adfobj.cam_hist_strs
     print("cam_hist_strs",cam_hist_strs,"\n")
-    case_match = [string for string in cam_hist_strs if "h0" in string][0]
+    case_match = [string for string in cam_hist_strs if "h0" in string]
+    print("which h0 were caught for the case:",case_match,"\n")
     baseline_hist_strs = adfobj.baseline_hist_strs
     print("baseline_hist_strs",baseline_hist_strs,"\n")
     base_match = [string for string in baseline_hist_strs if "h0" in string]
