68f5c090dfc1fd1bc9226db72ec60e67f4fa7049
justin-richling <richling@ucar.edu>
2024-07-08 13:44:47 -0600

    Update create_climo_files.py

--- scripts/averaging/create_climo_files.py
+++ scripts/averaging/create_climo_files.py
@@ -197,7 +197,7 @@ def process_variable(ts_files, syr, eyr, output_file):
     if len(ts_files) == 1:
         cam_ts_data = xr.open_dataset(ts_files[0], decode_times=True).compute()
     else:
-        cam_ts_data = xr.open_mfdataset(ts_files, decode_times=True, combine='by_coords').compute()
+        cam_ts_data = xr.open_mfdataset(ts_files, decode_times=True, combine='by_coords')#.compute()
     #Average time dimension over time bounds, if bounds exist:
     if 'time_bnds' in cam_ts_data:
         time = cam_ts_data['time']
