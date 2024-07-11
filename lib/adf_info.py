58fe719dd7bee93f6ae4a44775f419e779d85e7f
justin-richling <richling@ucar.edu>
2024-07-11 16:22:43 -0600

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -193,6 +193,9 @@ class AdfInfo(AdfConfig):
             #Get climo years for verification or assignment if missing
             baseline_hist_locs = self.get_baseline_info('cam_hist_loc')
 
+            # Read hist_str (component.hist_num, eg cam.h0) from the yaml file
+            baseline_hist_str = self.get_baseline_info("hist_str")
+
             #Check if user provided
             if not baseline_hist_str:
                 baseline_hist_str = ['cam.h0a']
