ed7623db30b00e25f4b528024d2ad3aed1ae7549
justin-richling <richling@ucar.edu>
2024-07-19 14:49:04 -0600

    asd

--- scripts/plotting/global_latlon_map.py
+++ scripts/plotting/global_latlon_map.py
@@ -166,6 +166,7 @@ def global_latlon_map(adfobj):
         odata = adfobj.data.load_reference_regrid_da(base_name, var)
 
         if odata is None:
+            print("odata NONE")
             dmsg = f"No regridded test file for {base_name} for variable `{var}`, global lat/lon mean plotting skipped."
             adfobj.debug_log(dmsg)
             continue
