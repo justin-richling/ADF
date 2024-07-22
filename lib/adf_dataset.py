c5398b6aed15f6096c65d108a1e4d0a93b7d2375
justin-richling <richling@ucar.edu>
2024-07-22 11:59:39 -0600

    Update adf_dataset.py

--- lib/adf_dataset.py
+++ lib/adf_dataset.py
@@ -319,7 +319,8 @@ class AdfData:
         res = self.adf.variable_defaults
         if variablename in res:
             vres = res[variablename]
-            if self.adf.compare_obs:
+            if (case == self.ref_labels[variablename]) and (self.adf.compare_obs):
+            #if self.adf.compare_obs:
                 scale_factor = vres.get("obs_scale_factor",1)
                 add_offset = vres.get("obs_add_offset", 0)
             else:
