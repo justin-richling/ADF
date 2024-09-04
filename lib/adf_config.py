401881122f6b4c26576a4796fdc231c054d4991a
justin-richling <richling@ucar.edu>
2024-09-04 12:03:26 -0600

    Update adf_config.py

--- lib/adf_config.py
+++ lib/adf_config.py
@@ -62,7 +62,8 @@ class AdfConfig(AdfBase):
             #Load YAML file:
             self.__config_dict = yaml.load(nfil, Loader=yaml.SafeLoader)
         
-        print("WTF",self.__config_dict,"\n")
+        print("\nWTF",self.__config_dict,"\n")
+        print(self.__config_dict.keys(),"\n")
 
         #Create search dictionary for variable expansion:
         self.__search_dict = self.__create_search_dict(self.__config_dict)
