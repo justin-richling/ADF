d7038768f026ee6015b11375acc37d841eaffa01
justin-richling <richling@ucar.edu>
2024-09-05 15:05:42 -0600

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -429,6 +429,7 @@ class AdfInfo(AdfConfig):
             if any(cam_hist_locs):
                 #Grab first possible hist string, just looking for years of run
                 hist_str = hist_str_case[0]
+                print("hist_str",hist_str,"\n")
 
                 #Get climo years for verification or assignment if missing
                 starting_location = Path(cam_hist_locs[case_idx])
