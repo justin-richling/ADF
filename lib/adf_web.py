20a77c1d74f9d07b2d133aee6db1cfb679cabc03
justin-richling <richling@ucar.edu>
2024-09-09 11:39:00 -0600

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -724,7 +724,8 @@ class AdfWeb(AdfObs):
                 if case_name in self.__case_web_paths:
                     #Extract website directory:
                     website_dir = self.__case_web_paths[case_name]['website_dir']
-                    if website_dir:
+                    print("website_dir",website_dir,"\n")
+                    if website_dir.is_dir():
 
                         #if (main_site_path / case_name):
                         #Copy website directory to "main site" directory:
