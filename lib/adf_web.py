d8ff975676e0becd339b356680c3ffc8dc167f44
justin-richling <richling@ucar.edu>
2024-09-29 17:34:58 -0600

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -673,8 +673,8 @@ class AdfWeb(AdfObs):
                     if web_data.season == "DJF":
                         #if web_data.case == self.data.ref_case_label:
                         if 1==1:
-                            print("web_data.name",web_data.name)
                             print("WOWSA:",mean_html_info[web_data.plot_type][web_data.category][var2]["DJF"],"\n\n\n")
+                print("web_data.name",web_data.name)
                 rend_kwarg_dict = {"title": main_title,
                                        "var_title": web_data.name,
                                        "season_title": web_data.season,
