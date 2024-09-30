d9bab146899e4a3a20b371d643ad1c0a361239f7
justin-richling <richling@ucar.edu>
2024-09-30 09:19:01 -0600

    Update aod_latlon.py

--- scripts/plotting/aod_latlon.py
+++ scripts/plotting/aod_latlon.py
@@ -653,5 +653,5 @@ def yeah_boi(adfobj, plotnames, plot_params, fields, season, obs_name, case_name
 
     for i in ind_web_dict.keys():
         print("HUH???",i)
-        for j in i.keys():
+        for j in ind_web_dict[i].keys():
             adfobj.add_website_data(**i[j])
