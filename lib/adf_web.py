61f36e36ac394dab372d15e8a757e85864b666e2
justin-richling <richling@ucar.edu>
2024-10-03 14:39:59 -0600

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -60,10 +60,12 @@ class _WebData:
                  season = None,
                  non_season = False,
                  plot_type = "Special",
+                 ext = "Mean",
                  data_frame = False,
                  html_file  = None,
                  asset_path = None,
-                 multi_case = False):
+                 multi_case = False,
+                 cat_sub = None):
 
         #Initialize relevant website variables:
         self.name       = web_name
@@ -73,10 +75,12 @@ class _WebData:
         self.season     = season
         self.non_season = non_season
         self.plot_type  = plot_type
+        self.ext        = ext
         self.data_frame = data_frame
         self.html_file  = html_file
         self.asset_path = asset_path
         self.multi_case = multi_case
+        self.cat_sub    = cat_sub
 
 #+++++++++++++++++++++
 #Define main web class
@@ -186,7 +190,9 @@ class AdfWeb(AdfObs):
                          season = None,
                          non_season = False,
                          plot_type = "Special",
-                         multi_case=False):
+                         ext = "Mean",
+                         multi_case=False,
+                         cat_sub=None):
 
         """
         Method that provides scripts a way to add an image file or
@@ -211,6 +217,8 @@ class AdfWeb(AdfObs):
 
         plot_type  -> Type of plot.  If not provided then plot type will be "Special".
 
+        ext        -> file name extension for averaged tag? ie Mean, or somethign else?
+
         multi_case -> Logical which indicates whether the image or dataframe can contain
                       multiple cases (e.g. a line plot with one line for each case).
 
@@ -227,7 +235,12 @@ class AdfWeb(AdfObs):
         #Check that the web_data is either a path
         #or a pandas dataframe:
         try:
+            #if cat_sub:
+            #    web_data = Path(cat_sub)
+            #else:
             web_data = Path(web_data)
+
+            #print("web_data",web_data,"\n")
 
             #Make sure the path is to an actual file:
             if not web_data.is_file():
@@ -284,6 +297,7 @@ class AdfWeb(AdfObs):
             #End if
             asset_path = None
         else:
+            #print("web_data.stem",web_data.stem,"\n\n")
             html_name = f'plot_page_{web_data.stem}.html'
             html_file = self.__case_web_paths[case_name]["img_pages_dir"] / html_name
             asset_path = self.__case_web_paths[case_name]['assets_dir'] / web_data.name
@@ -295,10 +309,12 @@ class AdfWeb(AdfObs):
                             season = season,
                             non_season = non_season,
                             plot_type = plot_type,
+                            ext = ext,
                             data_frame = data_frame,
                             html_file = html_file,
                             asset_path = asset_path,
-                            multi_case = multi_case)
+                            multi_case = multi_case,
+                            cat_sub=cat_sub)
 
         #Add web data object to list:
         self.__website_data.append(web_data)
@@ -339,6 +355,12 @@ class AdfWeb(AdfObs):
         # the use of 'list' in the html rendering.
         def jinja_list(seas_list):
             return list(seas_list)
+
+        def jinja_print(arg):
+            return print(arg)
+        
+        def jinja_enumerate(arg):
+            return enumerate(arg)
 
         #Notify user that script has started:
         print("\n  Generating Diagnostics webpages...")
@@ -520,7 +542,7 @@ class AdfWeb(AdfObs):
                     #End if
                 #End if
 
-                #Initialize Ordered Dictionary for plot type:
+                """#Initialize Ordered Dictionary for plot type:
                 if ptype not in mean_html_info:
                     mean_html_info[ptype] = OrderedDict()
                 #End if
@@ -580,7 +602,7 @@ class AdfWeb(AdfObs):
                 #formatting features.
                 table_html = web_data.data.to_html(index=False, border=1, justify='center',
                                                    float_format='{:6g}'.format)
-                table_html_info
+
                 #Construct amwg_table.html
                 rend_kwarg_dict = {"title": main_title,
                                   "case_name": case1,
@@ -634,6 +656,7 @@ class AdfWeb(AdfObs):
 
                 rend_kwarg_dict = {"title": main_title,
                                        "var_title": web_data.name,
+                                       "ext": web_data.ext,
                                        "season_title": web_data.season,
                                        "case_name": web_data.case,
                                        "case_yrs": case_yrs,
@@ -660,9 +683,14 @@ class AdfWeb(AdfObs):
                 mean_tmpl = jinenv.get_template('template_mean_diag.html')
 
                 #Remove keys from main dictionary for this html page
-                templ_rend_kwarg_dict = {k: rend_kwarg_dict[k] for k in rend_kwarg_dict.keys() - {'imgs', 'var_title', 'season_title'}}
+                #templ_rend_kwarg_dict = {k: rend_kwarg_dict[k] for k in rend_kwarg_dict.keys() - {'imgs', 'var_title', 'season_title'}}
+                templ_rend_kwarg_dict = {k: rend_kwarg_dict[k] for k in rend_kwarg_dict.keys() - {'imgs', 'season_title'}}
                 templ_rend_kwarg_dict["list"] = jinja_list
-                mean_rndr = mean_tmpl.render(templ_rend_kwarg_dict)
+                templ_rend_kwarg_dict["enumerate"] = jinja_enumerate
+                rend_kwarg_dict["enumerate"] = jinja_enumerate
+                rend_kwarg_dict["list"] = jinja_list
+                templ_rend_kwarg_dict["print"] = jinja_print
+                mean_rndr = mean_tmpl.render(rend_kwarg_dict)
 
                 #Write mean diagnostic plots HTML file:
                 with open(mean_ptype_file,'w', encoding='utf-8') as ofil:
