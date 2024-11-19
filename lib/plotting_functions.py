890a0d75efaa7676384fbca0173d1cc48924ff14
justin-richling <richling@ucar.edu>
2024-11-19 16:38:51 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -335,6 +335,27 @@ def global_average(fld, wgt, verbose=False):
     avg1, sofw = np.ma.average(fld2, axis=a, weights=wgt, returned=True) # sofw is sum of weights
 
     return np.ma.average(avg1)
+#######
+
+
+def coslat_average(darray, lat1, lat2):
+    """
+    Calculate the weighted average for an [:,lat] array over the region
+    lat1 to lat2
+    """
+
+    # flip latitudes if they are decreasing
+    if (darray.lat[0] > darray.lat[darray.lat.size -1]):
+        print("flipping latitudes")
+        darray = darray.sortby('lat')
+
+    region = darray.sel(lat=slice(lat1, lat2))
+    weights=np.cos(np.deg2rad(region.lat))
+    regionw = region.weighted(weights)
+    regionm = regionw.mean("lat")
+
+    return regionm
+#######
 
 
 def spatial_average(indata, weights=None, spatial_dims=None):
