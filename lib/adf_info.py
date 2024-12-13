58f46490286cab47b1620ae9ce3b08eaffb8f8e8
justin-richling <richling@ucar.edu>
2024-12-13 16:56:19 -0700

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -529,7 +529,8 @@ class AdfInfo(AdfConfig):
         eyears_fixed = []
         for case_idx, case_name in enumerate(case_names):
 
-            if not calc_test_ts[case_idx]:
+            #if not calc_test_ts[case_idx]:
+            if not calc_test_ts[case_names]:
                 cam_hist_locs[case_idx] = None
                 #test_ts_loc = test_ts_locs[case_idx]
 
