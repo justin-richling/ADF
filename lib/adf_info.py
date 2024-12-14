8fecac48344e420b902612273226a994cc7bf020
justin-richling <richling@ucar.edu>
2024-12-13 17:05:44 -0700

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -585,8 +585,8 @@ class AdfInfo(AdfConfig):
             #Check if history file path exists:
             hist_str_case = hist_str[case_idx]
             print("cam_hist_locs",cam_hist_locs,"\n")
-            #if any(cam_hist_locs):
-            if cam_hist_locs[case_idx]:
+            if any(cam_hist_locs):
+            #if cam_hist_locs[case_idx]:
                 #Grab first possible hist string, just looking for years of run
                 hist_str = hist_str_case[0]
 
