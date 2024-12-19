5eae70d1e2d78d1c0ab168dea8cf8f330fb380df
justin-richling <richling@ucar.edu>
2024-12-19 15:10:48 -0700

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -236,8 +236,8 @@ def tem(adf):
                 #Gather data for both cases
                 mdata = ds[var].squeeze()
                 odata = ds_base[var].squeeze()
-                print("ds.shape",type(mdata))
-                print("ds_base.shape",type(odata))
+                print("ds.shape",mdata.shape)
+                print("ds_base.shape",odata.shape)
                 odata = interp_tem(mdata, odata)
                 
 
