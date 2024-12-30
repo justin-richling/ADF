72302afd814aea646a5f18c5c6dcd6d79a0143aa
justin-richling <richling@ucar.edu>
2024-12-30 14:58:04 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -817,7 +817,13 @@ def make_polar_plot(wks, case_nickname, base_nickname,
         pct_cyclic = pct_cyclic.clip(min=-100, max=100)
         print("\nasdasdadssadasd",pct_cyclic.isel(dim_0=0).isel(dim_1=0))
         print("QWTF",pct_cyclic)
-        img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm, levels=levelspctdiff)
+        from shapely.errors import GEOSException  # Import shapely
+        try:
+            img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm, levels=levelspctdiff)
+        except GEOSException as e:
+            print("Caught GEOSException:", e)
+            img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), colors="w", norm=pctnorm)
+            ax3.text(0.4, 0.4, empty_message, transform=ax3.transAxes, bbox=props)
         '''try:
             img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm, levels=levelspctdiff)#, transform_first=True
         except TypeError:
