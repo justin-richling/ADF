cd4118d23623b5c27ebaae1671f9ce9d3861f60d
justin-richling <richling@ucar.edu>
2024-12-30 13:21:25 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -811,7 +811,7 @@ def make_polar_plot(wks, case_nickname, base_nickname,
         ax3.text(0.4, 0.4, empty_message, transform=ax3.transAxes, bbox=props)
     else:
         pct_cyclic = xr.DataArray(pct_cyclic)
-        pct_cyclic = pct_cyclic.where(pct_cyclic >= 0.1, 0)
+        pct_cyclic = pct_cyclic.where(pct_cyclic < 0, 0)
         img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm, levels=levelspctdiff)
         '''try:
             img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm, levels=levelspctdiff)#, transform_first=True
