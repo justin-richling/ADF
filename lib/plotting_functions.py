cb15b8a5799ccc58a6aeb739adf329f8157bf81b
justin-richling <richling@ucar.edu>
2024-12-30 18:43:43 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -818,7 +818,7 @@ def make_polar_plot(wks, case_nickname, base_nickname,
         print("\nasdasdadssadasd",pct_cyclic.isel(dim_0=0).isel(dim_1=0))
         print("QWTF",pct_cyclic,"\n")
         img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm, levels=levelspctdiff)
-        '''
+        
         from shapely.errors import GEOSException  # Import shapely
         try:
             img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm, levels=levelspctdiff)
@@ -833,7 +833,7 @@ def make_polar_plot(wks, case_nickname, base_nickname,
             ax3.text(0.4, 0.4, empty_message, transform=ax3.transAxes, bbox=props)
             no_cbar = True
             #ax3.text(0.4, 0.4, empty_message, transform=ax3.transAxes, bbox=props)
-        '''
+
     if len(levs_diff) < 2:
         img4 = ax4.contourf(lons, lats, dif_cyclic, transform=ccrs.PlateCarree(), colors="w", norm=dnorm)
         ax4.text(0.4, 0.4, empty_message, transform=ax4.transAxes, bbox=props)
