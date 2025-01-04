9151f6c785445d2e19fd9250747cb24136ef080b
justin-richling <richling@ucar.edu>
2025-01-03 17:15:36 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -835,7 +835,7 @@ def make_polar_plot(wks, case_nickname, base_nickname,
         try:
             img3 = ax3.contourf(plons, plats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm,
                                 levels=levelspctdiff,
-                                transform_first=True
+                                #transform_first=True
                                 )#, transform_first=True
 
         except (GEOSException, ValueError, TypeError) as e:
