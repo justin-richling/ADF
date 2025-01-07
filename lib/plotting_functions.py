bc7faa682cf398ccdfccdc7a9f976211d61d8ec1
justin-richling <richling@ucar.edu>
2025-01-06 17:39:33 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -2296,8 +2296,8 @@ def plot_zonal_mean_and_save(wks, case_nickname, base_nickname,
         fig.legend(handles=[line,line2],
                    #bbox_to_anchor=(-0.15, 0.87, 1.05, .102),
                    loc="upper left",
-                   borderaxespad=0.0,fontsize=4,frameon=False,
-                   bbox_to_anchor=(.125, 0.87), bbox_transform=fig.transFigure)
+                   borderaxespad=0.0,fontsize=6,frameon=False,
+                   bbox_to_anchor=(.125, 0.92), bbox_transform=fig.transFigure)
 
         zonal_plot(adata['lat'], diff, ax=ax[1], color="k")
         ax[1].set_title("$\mathbf{Test} - \mathbf{Baseline}$", loc='left', fontsize=6)
