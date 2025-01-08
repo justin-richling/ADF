d2d19d597f7fed7cc6d5712ee81cb9e5d99891cc
justin-richling <richling@ucar.edu>
2025-01-08 10:36:31 -0700

    Update global_mean_timeseries.py

--- scripts/plotting/global_mean_timeseries.py
+++ scripts/plotting/global_mean_timeseries.py
@@ -237,11 +237,11 @@ class Lens2Data:
             f"/glade/campaign/cgd/cas/islas/CESM_DATA/LENS2/global_means/annualmeans/"
         )
 
-        lens2_fil = sorted(lens2_path.glob(f"{self.field}*first50*nc"))
+        lens2_fil = sorted(lens2_path.glob(f"{self.field}_*first50*nc"))
         print(self.field,lens2_fil)
         if lens2_fil:
             print(self.field,lens2_fil)
-            lens2_fil = sorted(lens2_path.glob(f"{self.field}*first50*nc"))[0]
+            lens2_fil = lens2_fil[0]
         #else:
         #    lens2_fil = ''
 
