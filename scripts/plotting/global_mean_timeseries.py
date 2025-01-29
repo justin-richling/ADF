ddb20b0869af613fff9065baf378e0407e390ffb
justin-richling <richling@ucar.edu>
2025-01-29 09:27:01 -0700

    Update global_mean_timeseries.py

--- scripts/plotting/global_mean_timeseries.py
+++ scripts/plotting/global_mean_timeseries.py
@@ -146,7 +146,7 @@ def global_mean_timeseries(adfobj):
                     field
                 )  # Provides access to LENS2 dataset when available (class defined below)
             else:
-                print(f"Model years for '{field}' are outside LENS years, will skip plotting LENS data for clarity boi")
+                print(f"Some model years for '{field}' are outside LENS years, will skip plotting LENS data for clarity boi")
                 lens2_data = None
 
             c_ts_da = adfobj.data.load_timeseries_da(case_name, field)
