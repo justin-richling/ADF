066eff803eae7c6cd5810431205420d4b7b22945
justin-richling <richling@ucar.edu>
2025-01-29 16:47:29 -0700

    Update tape_recorder.py

--- scripts/plotting/tape_recorder.py
+++ scripts/plotting/tape_recorder.py
@@ -194,7 +194,7 @@ def tape_recorder(adfobj):
         print("ts_loc",var,"\n")
         fils = sorted(ts_loc.glob(f'*{hist_str}.{var}.*.nc'))
         #dat = adfobj.data.load_timeseries_dataset(fils, start_years[idx], end_years[idx])
-        dat = adfobj.data.load_timeseries_da(case_names[idx], var, start_years[idx], end_years[idx])
+        dat = adfobj.data.load_da(fils, var, start_years[idx], end_years[idx])
         print("\n\n",dat,"\n\n")
         if not dat:
             dmsg = f"\t No data for `{var}` found in {fils}, case will be skipped in tape recorder plot."
