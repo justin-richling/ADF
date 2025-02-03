ed950de4a69f97a6adafb51e2120c90ac23110e7
justin-richling <richling@ucar.edu>
2025-02-03 14:19:33 -0700

    Update create_climo_files.py

--- scripts/averaging/create_climo_files.py
+++ scripts/averaging/create_climo_files.py
@@ -175,10 +175,12 @@ def create_climo_files(adf, clobber=False, search=None):
             ts_filenames = search.format(CASE=case_name, HIST_STR="h0", VARIABLE=var)
             ts_files = sorted(list(input_location.glob(ts_filenames)))
 
+            print("AH", case_name, ts_files,"\n")
+
             #If no files exist, try to move to next variable. --> Means we can not proceed with this variable,
             # and it'll be problematic later unless there are multiple hist file streams and the variable is in the others
             if not ts_files:
-                errmsg = f"\t  **Time series files for variable '{var}' not found.  Script will continue to next variable."
+                errmsg = f"\t    WARNING: Time series files for variable '{var}' not found.  Script will continue to next variable."
                 logmsg = f"climo file generation: The input location searched was: {input_location}. The glob pattern was {ts_filenames}."
                 #Write to debug log if enabled:
                 adf.debug_log(logmsg)
