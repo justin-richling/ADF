549668f7ed7996d01c6d945c29cf13f4c086df4f
justin-richling <richling@ucar.edu>
2025-02-07 17:16:07 -0700

    Update regrid_and_vert_interp_tem.py

--- scripts/regridding/regrid_and_vert_interp_tem.py
+++ scripts/regridding/regrid_and_vert_interp_tem.py
@@ -306,6 +306,7 @@ def regrid_and_vert_interp_tem(adf):
                     print("mhist_loc",mhist_loc)
                     #Generate CAM climatology (climo) file list:
                     mhist_fils = sorted(mhist_loc.glob(f"{case_name}.h4.*.nc"))
+                    print("mhist_fils",mhist_fils)
 
                     if len(mhist_fils) > 1:
                         #Combine all cam files together into a single data set:
