0ec2be57cc0d33574e55d0e7733590adb1fa687e
justin-richling <richling@ucar.edu>
2025-02-10 16:47:30 -0700

    Update qbo.py

--- scripts/plotting/qbo.py
+++ scripts/plotting/qbo.py
@@ -131,16 +131,18 @@ def qbo(adfobj):
     #casedat = [pf.load_dataset(sorted(Path(case_loc[i]).glob(f"{case_names[i]}.*.U.*.nc"))) for i in range(0,ncases,1)]
 
     casedat = []
-    # Get baseline data
-    cam_ts_data = pf.load_dataset(sorted(Path(base_loc).glob(f"{base_name}.*.U.*.nc")))
-    if cam_ts_data:
-        ncases = 1
+    
+    # Get baseline data if applicable
+    if not adfobj.compare_obs:
+        cam_ts_data = pf.load_dataset(sorted(Path(base_loc).glob(f"{base_name}.*.U.*.nc")))
+        if cam_ts_data:
+            ncases = 1
 
-        tslice = adfobj.data.get_time_slice_by_year(cam_ts_data.time, int(bl_syr), int(bl_eyr))
-        cam_ts_data = cam_ts_data.isel(time=tslice)
-        casedat.append(cam_ts_data)
-    else:
-        ncases = 0
+            tslice = adfobj.data.get_time_slice_by_year(cam_ts_data.time, int(bl_syr), int(bl_eyr))
+            cam_ts_data = cam_ts_data.isel(time=tslice)
+            casedat.append(cam_ts_data)
+        else:
+            ncases = 0
 
     # Loop over test case data
     #for i in range(0,ncases,1):
