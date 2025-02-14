91ed2c895e8bd56589081a92f81c25de14f99a5d
justin-richling <richling@ucar.edu>
2025-02-14 15:36:27 -0700

    Update create_climo_files.py

--- scripts/averaging/create_climo_files.py
+++ scripts/averaging/create_climo_files.py
@@ -242,10 +242,12 @@ def create_climo_files(adf, clobber=False, search=None):
             # Parallelize the computation using multiprocessing pool:
             with mp.Pool(processes=number_of_cpu) as p:
                 result = p.starmap(process_variable, list_of_arguments)
+        else:
+            
 
-        # Parallelize the computation using multiprocessing pool:
-        with mp.Pool(processes=number_of_cpu) as p:
-            result = p.starmap(process_variable, list_of_arguments)
+        ## Parallelize the computation using multiprocessing pool:
+        #with mp.Pool(processes=number_of_cpu) as p:
+        #    result = p.starmap(process_variable, list_of_arguments)
 
     #End of model case loop
     #----------------------
