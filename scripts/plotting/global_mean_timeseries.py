b9399f98eb261fa70f52439e7ccd90142cb14def
justin-richling <richling@ucar.edu>
2025-03-17 16:25:51 -0600

    Update global_mean_timeseries.py

--- scripts/plotting/global_mean_timeseries.py
+++ scripts/plotting/global_mean_timeseries.py
@@ -85,7 +85,7 @@ def global_mean_timeseries(adfobj):
             # check if there is a lat dimension:
             if not has_lat_ref:
                 regrd_ts_loc = adfobj.get_baseline_info("cam_ts_regrid_loc")
-                ref_ts_da = xr.opend_dataset(sorted(Path(regrd_ts_loc).glob(f"*{field}*.nc"))[0])
+                ref_ts_da = xr.open_dataset(sorted(Path(regrd_ts_loc).glob(f"*{field}*.nc"))[0])
                 has_lat_ref, has_lev_ref = pf.zm_validate_dims(ref_ts_da)
                 if not has_lat_ref:
                     print(
@@ -147,7 +147,7 @@ def global_mean_timeseries(adfobj):
             # check if there is a lat dimension:
             if not has_lat_case:
                 regrd_case_ts_loc = regrd_case_ts_locs[idx]
-                c_ts_da = xr.opend_dataset(sorted(Path(regrd_case_ts_loc).glob(f"*{field}*.nc"))[0])
+                c_ts_da = xr.open_dataset(sorted(Path(regrd_case_ts_loc).glob(f"*{field}*.nc"))[0])
                 has_lat_case, has_lev_case = pf.zm_validate_dims(c_ts_da)
                 if not has_lat_case:
                     print(
