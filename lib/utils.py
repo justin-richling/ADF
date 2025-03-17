5993622234177d70f5455cc74864d9d8ae41ac4c
justin-richling <richling@ucar.edu>
2025-03-17 14:09:58 -0600

    Update utils.py

--- lib/utils.py
+++ lib/utils.py
@@ -278,7 +278,15 @@ def unstructure_regrid(model_dataset, var_name, comp, weight_file, latlon_file, 
         d_data = fv_ds.landmask
     else:
         s_data = mdata.isel(time=0)
-        d_data = fv_ds[list(fv_ds.variables)[0]]
+        # Get the first data variable with 'time' and 'ncol' dimensions
+        matching_vars = [var for var in ds.data_vars if set(ds[var].dims) == {'time', 'lndgrid'}]
+
+        if matching_vars:
+            first_matching_var = matching_vars[0]
+            print(f"First matching variable: {first_matching_var}")
+            first_var_data = ds[first_matching_var]
+            print(first_var_data.name)
+        d_data = fv_ds[first_matching_var]
 
     print("\nWeights file? ",weight_file,"\n")
 
