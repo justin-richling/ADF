d69cb456794c086839a3d7dd543c4bfaa1c2e1d7
justin-richling <richling@ucar.edu>
2025-03-21 10:48:43 -0600

    Update adf_dataset.py

--- lib/adf_dataset.py
+++ lib/adf_dataset.py
@@ -294,7 +294,7 @@ class AdfData:
     def get_regrid_file(self, case, field):
         """Return list of test regridded files"""
         #model_rg_loc = Path(self.adf.get_basic_info("cam_climo_regrid_loc", required=True))
-        model_rg_locs = self.adf.get_cam_info("cam_regrid_loc", required=True)
+        model_rg_locs = self.adf.diag_basic_info("cam_regrid_loc", required=True)
         caseindex = (self.case_names).index(case) # the entry for specified case
         model_rg_loc = Path(model_rg_locs[caseindex])
         rlbl = self.ref_labels[field]  # rlbl = "reference label" = the name of the reference data that defines target grid
