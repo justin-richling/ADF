d624e353938e253460e4606b4eadfd292500712e
justin-richling <richling@ucar.edu>
2025-03-25 09:32:07 -0600

    Update utils.py

--- lib/utils.py
+++ lib/utils.py
@@ -207,12 +207,12 @@ def unstructure_regrid(model_dataset, var_name, comp, weight_file, method, latlo
     regridder = make_se_ts_regridder(weight_file=weight_file,
                                     Method = method,
                                     )
-    if method == 'coservative':
-        rgdata = regrid_lnd_se_data_conservative(regridder, model_dataset, comp_grid)
-    if method == 'bilinear':
-        rgdata = regrid_lnd_se_data_bilinear(regridder, model_dataset, comp_grid)
 
     if comp == "lnd":
+        if method == 'coservative':
+            rgdata = regrid_lnd_se_data_conservative(regridder, model_dataset, comp_grid)
+        if method == 'bilinear':
+            rgdata = regrid_lnd_se_data_bilinear(regridder, model_dataset, comp_grid)
         rgdata[var_name] = (rgdata[var_name] / rgdata.landfrac)
         rgdata['landmask'] = fv_ds.landmask
         rgdata['landfrac'] = rgdata.landfrac#.isel(time=0)
@@ -246,53 +246,6 @@ def unstructure_regrid(model_dataset, var_name, comp, weight_file, method, latlo
 
     #Return dataset:
     return rgdata
-
-
-
-'''def make_se_regridder(weight_file, Method='conservative'):
-    weights = xr.open_dataset(weight_file)
-    in_shape = weights.src_grid_dims.load().data
-
-    # Since xESMF expects 2D vars, we'll insert a dummy dimension of size-1
-    if len(in_shape) == 1:
-        in_shape = [1, in_shape.item()]
-
-    # output variable shape
-    out_shape = weights.dst_grid_dims.load().data.tolist()[::-1]
-
-    dummy_in = xr.Dataset(
-        {
-            "lat": ("lat", np.empty((in_shape[0],))),
-            "lon": ("lon", np.empty((in_shape[1],))),
-        }
-    )
-    dummy_out = xr.Dataset(
-        {
-            "lat": ("lat", weights.yc_b.data.reshape(out_shape)[:, 0]),
-            "lon": ("lon", weights.xc_b.data.reshape(out_shape)[0, :]),
-        }
-    )
-    # Apparently not needed ... maybe for Method=conservative ?
-    # # Hard code masks for now, not sure this does anything?
-    # s_mask = xr.DataArray(s_data.data.reshape(in_shape[0],in_shape[1]), dims=("lat", "lon"))
-    # dummy_in['mask']= s_mask
-    
-    # d_mask = xr.DataArray(d_data.values, dims=("lat", "lon"))  
-    # dummy_out['mask']= d_mask                
-
-    # do source and destination grids need masks here?
-    # See xesmf docs https://xesmf.readthedocs.io/en/stable/notebooks/Masking.html#Regridding-with-a-mask
-    regridder = xesmf.Regridder(
-        dummy_in,
-        dummy_out,
-        weights=weight_file,
-        # results seem insensitive to this method choice
-        # choices are coservative_normed, coservative, and bilinear
-        method=Method,
-        reuse_weights=True,
-        periodic=True,
-    )
-    return regridder'''
 
 
 def regrid_atm_se_data_bilinear(regridder, data_to_regrid, comp_grid='ncol'):
