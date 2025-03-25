ee9dcf0c996acba503a54ed18e87384b68eae5a1
justin-richling <richling@ucar.edu>
2025-03-25 15:47:19 -0600

    Update create_climo_files.py

--- scripts/averaging/create_climo_files.py
+++ scripts/averaging/create_climo_files.py
@@ -210,7 +210,7 @@ def create_climo_files(adf, clobber=False, search=None):
                     if sorted(regrd_ts_loc.glob(f"*.{var}.*nc")):
                         #ts_ds = xr.open_dataset(sorted(regrd_ts_loc.glob(f"*.{var}.*nc"))[0])
                         ts_files = sorted(regrd_ts_loc.glob(f"*.{var}.*nc"))"""
-            if (adf_utils.check_unstructured(cam_ts_data, case_name)) or (not ts_files):
+            """if (adf_utils.check_unstructured(cam_ts_data, case_name)) or (not ts_files):
                 if is_baseline:
                     ts_dir = Path(input_ts_baseline)
                     regrd_ts_loc = ts_dir / "regrid"
@@ -225,7 +225,7 @@ def create_climo_files(adf, clobber=False, search=None):
                         ts_files = adf.data.get_ref_timeseries_file(var)
                     else:
                         ts_files = adf.data.get_timeseries_file(case_name, var)
-                #ts_files = sorted(regrd_ts_loc.glob(f"*.{var}.*nc"))
+                #ts_files = sorted(regrd_ts_loc.glob(f"*.{var}.*nc"))"""
                 
 
             #If no files exist, try to move to next variable. --> Means we can not proceed with this variable,
