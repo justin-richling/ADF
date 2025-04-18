b68d11e77972dc91334ed93264d02936c451b0e5
justin-richling <richling@ucar.edu>
2025-04-18 11:41:49 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1526,6 +1526,8 @@ def lev_to_plev(data, ps, hyam, hybm, P0=100000., new_levels=None,
     #Temporary print statement to notify users to ignore warning messages.
     #This should be replaced by a debug-log stdout filter at some point:
     print("Please ignore the interpolation warnings that follow!")
+    if 'zalat' in data.dims:
+        data = data.rename({'zalat': 'lat'})
 
     #Apply GeoCAT hybrid->pressure interpolation:
     if new_levels is not None:
