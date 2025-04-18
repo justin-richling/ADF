2f35b994787252a6c0057ee78102e408f64a76a2
justin-richling <richling@ucar.edu>
2025-04-18 11:46:19 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1528,6 +1528,8 @@ def lev_to_plev(data, ps, hyam, hybm, P0=100000., new_levels=None,
     print("Please ignore the interpolation warnings that follow!")
     if 'zalat' in data.dims:
         data = data.rename({'zalat': 'lat'})
+        data = data.expand_dims({'lon': 1})
+        ps=ps.expand_dims({'lon': 1})
 
     #Apply GeoCAT hybrid->pressure interpolation:
     if new_levels is not None:
