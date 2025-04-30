22c31b7fbe73fa436f02db2ff360616b30bddbde
justin-richling <richling@ucar.edu>
2025-04-30 17:18:57 -0600

    Update create_TEM_files.py

--- scripts/averaging/create_TEM_files.py
+++ scripts/averaging/create_TEM_files.py
@@ -324,6 +324,24 @@ def create_TEM_files(adf):
 
             # write output to a netcdf file
             print("\n\ndstem0",dstem0["PS"],"\n\n")
+            #Average time dimension over time bounds, if bounds exist:
+            if 'time_bnds' in dstem0:
+                time_bounds_name = 'time_bnds'
+            elif 'time_bounds' in dstem0:
+                time_bounds_name = 'time_bounds'
+            else:
+                time_bounds_name = None
+
+            if time_bounds_name:
+                time = dstem0['time']
+                #NOTE: force `load` here b/c if dask & time is cftime,
+                #throws a NotImplementedError:
+
+                time = xr.DataArray(dstem0[time_bounds_name].load().mean(dim='nbnd').values,
+                                    dims=time.dims, attrs=time.attrs)
+                dstem0['time'] = time
+                dstem0.assign_coords(time=time)
+                dstem0 = xr.decode_cf(dstem0)
             dstem0.to_netcdf(tem_fil, unlimited_dims='time', mode='w')
 
         #End if (file creation or over-write file)
