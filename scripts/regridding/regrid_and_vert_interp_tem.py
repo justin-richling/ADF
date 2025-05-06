61687619ba456df850d87a6e34176dbafe742f97
justin-richling <richling@ucar.edu>
2025-05-05 21:18:56 -0600

    Update regrid_and_vert_interp_tem.py

--- scripts/regridding/regrid_and_vert_interp_tem.py
+++ scripts/regridding/regrid_and_vert_interp_tem.py
@@ -652,11 +652,11 @@ def _regrid_and_interpolate_levs(model_dataset, var_name, regrid_dataset=None, r
             983.19191142, 991.20714452, 997.52772561])
 
         # Generate 30 levels between 1000 and 3 hPa in log space
-        print("regrid_dataset",regrid_dataset,"\n")
+        #print("regrid_dataset",regrid_dataset,"\n")
         new_levs = False
         if regrid_dataset:
             new_levs = True
-            print("min(regrid_dataset.lev.values)",min(regrid_dataset.lev.values),"\n")
+            #print("min(regrid_dataset.lev.values)",min(regrid_dataset.lev.values),"\n")
             #np.array(regrid_dataset.lev.values)
             new_levels = np.array(np.logspace(np.log10(1000), np.log10(min(regrid_dataset.lev.values)), num=30))*100
 
