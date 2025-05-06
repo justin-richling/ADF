44f2a41ec58811d2191745de67b130f0fd5a226f
justin-richling <richling@ucar.edu>
2025-05-05 20:25:22 -0600

    Update regrid_and_vert_interp_tem.py

--- scripts/regridding/regrid_and_vert_interp_tem.py
+++ scripts/regridding/regrid_and_vert_interp_tem.py
@@ -654,14 +654,17 @@ def _regrid_and_interpolate_levs(model_dataset, var_name, regrid_dataset=None, r
 
         # Generate 30 levels between 1000 and 3 hPa in log space
         print("regrid_dataset",regrid_dataset,"\n")
-        print("min(regrid_dataset.lev.values)",min(regrid_dataset.lev.values),"\n")
-        new_levels = np.logspace(np.log10(1000), np.log10(min(regrid_dataset.lev.values)), num=30)
+        new_levs = False
+        if regrid_dataset:
+            new_levs = True
+            print("min(regrid_dataset.lev.values)",min(regrid_dataset.lev.values),"\n")
+            new_levels = np.logspace(np.log10(1000), np.log10(min(regrid_dataset.lev.values)), num=30)
 
-        # Optional: round for readability
-        new_levels = np.round(new_levels, decimals=3)
+            # Optional: round for readability
+            new_levels = np.round(new_levels, decimals=3)
         if vert_coord_type == "hybrid":
             #Interpolate from hybrid sigma-pressure to the standard pressure levels:
-            rgdata_interp = pf.lev_to_plev(rgdata, rg_ps, mhya, mhyb, P0=P0, \
+            rgdata_interp = pf.lev_to_plev(rgdata, rg_ps, mhya, mhyb, P0=P0, new_levels=new_levs, \
                                            convert_to_mb=True)
         elif vert_coord_type == "height":
             #Interpolate variable using mid-level pressure (PMID):
