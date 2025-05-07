801cac3dd7a424230ba5db11a0a7d0f3abefc456
justin-richling <richling@ucar.edu>
2025-05-07 14:04:05 -0600

    Update regrid_and_vert_interp.py

--- scripts/regridding/regrid_and_vert_interp.py
+++ scripts/regridding/regrid_and_vert_interp.py
@@ -276,7 +276,7 @@ def regrid_and_vert_interp(adf):
                                 raise AdfError(msg)
 
                             case_method = case_methods[case_idx]
-                            print("ds attrs?",mclim_ds.U.long_name,"\n\n")
+                            print("ds attrs?",mclim_ds.lev.long_name,"\n\n")
                             ds_attrs = mclim_ds.attrs
                             # Grid unstructured climo if applicable before regridding
                             rgdata_interp = _regrid(mclim_ds, var,
