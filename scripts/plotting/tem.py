90933beebe6af701c8d0fd11e9b91ded1584bb70
justin-richling <richling@ucar.edu>
2025-05-22 17:47:18 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -368,6 +368,18 @@ def tem(adf):
                 else:
                     #print("Mismatched shape between model and baseline, will skip difference plots but still show the data?")
                     dseasons = None"""
+
+                #print(mseasons,"\n")
+                if 'lat' in mseasons.dims:
+                    mseasons = mseasons.isel(lat=0)
+                if 'lat' in oseasons.dims:
+                    oseasons = oseasons.isel(lat=0)
+                #difference: each entry should be (lat, lon)
+                if (mseasons.dims == oseasons.dims) and (mseasons.shape == oseasons.shape):
+                    dseasons = mseasons-oseasons
+                else:
+                    #print("Mismatched shape between model and baseline, will skip difference plots but still show the data?")
+                    dseasons = None
                 
                 #Gather contour plot options
                 cp_info = pf.prep_contour_plot(mseasons, oseasons, dseasons, **vres)
@@ -400,17 +412,6 @@ def tem(adf):
                 ax = [ax1,ax2,ax3]
 
                 #Contour fill
-                #print(mseasons,"\n")
-                if 'lat' in mseasons.dims:
-                    mseasons = mseasons.isel(lat=0)
-                if 'lat' in oseasons.dims:
-                    oseasons = oseasons.isel(lat=0)
-                #difference: each entry should be (lat, lon)
-                if (mseasons.dims == oseasons.dims) and (mseasons.shape == oseasons.shape):
-                    dseasons = mseasons-oseasons
-                else:
-                    #print("Mismatched shape between model and baseline, will skip difference plots but still show the data?")
-                    dseasons = None
                 img0 = ax[0].contourf(mlats, mlevs, mseasons, levels=clevs, norm=norm, cmap=cmap)
                 img1 = ax[1].contourf(olats, olevs, oseasons, levels=clevs, norm=norm, cmap=cmap)
                     
