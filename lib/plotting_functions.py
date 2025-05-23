2476c7972e3b93f292cd61bb438e7e45d5f460c3
justin-richling <richling@ucar.edu>
2025-05-23 10:03:25 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1916,7 +1916,7 @@ def prep_contour_plot(adata, bdata, diffdata, pctdata=None, **kwargs):
             cmap_obj = cm.get_cmap(cmap1)
             norm1 = mpl.colors.BoundaryNorm(levels1, cmap_obj.N, extend='both')
         else:
-            norm1 = mpl.colors.Normalize(vmin=min(levels1), vmax=max(levels1), extend='both')
+            norm1 = mpl.colors.Normalize(vmin=min(levels1), vmax=max(levels1))
     elif 'contour_levels_range' in kwargs: # arange: Check if the user wants to generate a list from start, stop, step
         assert len(kwargs['contour_levels_range']) == 3, \
         "contour_levels_range must have exactly three entries: min, max, step"
@@ -1926,14 +1926,14 @@ def prep_contour_plot(adata, bdata, diffdata, pctdata=None, **kwargs):
             cmap_obj = cm.get_cmap(cmap1)
             norm1 = mpl.colors.BoundaryNorm(levels1, cmap_obj.N, extend='both')
         else:
-            norm1 = mpl.colors.Normalize(vmin=min(levels1), vmax=max(levels1), extend='both')
+            norm1 = mpl.colors.Normalize(vmin=min(levels1), vmax=max(levels1))
     else: # linspace: Check if user wants to generate a list from start, stop, num_steps
         levels1 = np.linspace(minval, maxval, 12)
         if ('non_linear_levels' in kwargs) and (kwargs['non_linear_levels']):
             cmap_obj = cm.get_cmap(cmap1)
             norm1 = mpl.colors.BoundaryNorm(levels1, cmap_obj.N, extend='both')
         else:
-            norm1 = mpl.colors.Normalize(vmin=minval, vmax=maxval, extend='both')
+            norm1 = mpl.colors.Normalize(vmin=minval, vmax=maxval)
     #End if
 
     #Check if the minval and maxval are actually different.  If not,
@@ -1946,9 +1946,9 @@ def prep_contour_plot(adata, bdata, diffdata, pctdata=None, **kwargs):
 
     if ('colormap' not in kwargs) and ('contour_levels' not in kwargs):
         if ((minval < 0) and (0 < maxval)) and mplv > 2:
-            norm1 = normfunc(vmin=minval, vmax=maxval, vcenter=0.0, extend='both')
+            norm1 = normfunc(vmin=minval, vmax=maxval, vcenter=0.0)
         else:
-            norm1 = mpl.colors.Normalize(vmin=minval, vmax=maxval, extend='both')
+            norm1 = mpl.colors.Normalize(vmin=minval, vmax=maxval)
         #End if
     #End if
 
