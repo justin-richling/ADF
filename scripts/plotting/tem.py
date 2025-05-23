4ba53045489f61ce98b1a72819a0a2d5f4bf6674
justin-richling <richling@ucar.edu>
2025-05-23 10:07:58 -0600

    Update tem.py

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -420,7 +420,7 @@ def tem(adf):
 
                 #Contour fill
                 if var == "TZM":
-                    clevs = np.arange(180,300)
+                    clevs = np.arange(180,301,5)
                 img0 = ax[0].contourf(mlats, mlevs, mseasons, levels=clevs, norm=norm, cmap=cmap)
                 img1 = ax[1].contourf(olats, olevs, oseasons, levels=clevs, norm=norm, cmap=cmap)
                     
