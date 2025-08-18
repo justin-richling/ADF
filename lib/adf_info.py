4f6f9d14eb609c0354b7a5becd1c0b3af2656127
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-08-18 13:42:51 -0600

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -82,12 +82,13 @@ class AdfInfo(AdfConfig):
 
         # Add CVDP info to object:
         self.__cvdp_info = self.read_config_var("diag_cvdp_info")
-
+        print("self.__cvdp_info",self.__cvdp_info)
         # Expand CVDP climo info variable strings:
         if self.__cvdp_info is not None:
-            cvdp_default_loc = Path("externals/CVDP/")
-            self.__cvdp_info.get("cvdp_codebase_loc",cvdp_default_loc)
+            #cvdp_default_loc = Path("externals/CVDP/")
+            #self.__cvdp_info.get("cvdp_codebase_loc",cvdp_default_loc)
             self.expand_references(self.__cvdp_info)
+            print("self.__cvdp_info",self.__cvdp_info)
 
             #cvdp_default_loc = Path("externals/CVDP/")
             #self.__cvdp_info.get("cvdp_loc",cvdp_default_loc)
