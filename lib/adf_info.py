07798b024f4728f65d10a565d21426f3a6c07690
justin-richling <richling@ucar.edu>
2025-08-19 14:04:11 -0600

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -426,9 +426,9 @@ class AdfInfo(AdfConfig):
 
                 if (len(case_names) > 1) or (isinstance(hist_strs, dict)):
                     #hist_str_case_1 = list(hist_strs.keys())[case_idx]
-                    hist_str_case_1 = hist_strs[case_idx]
+                    hist_str_case_1 = hist_strs#[case_idx]
                     print("hist_str_case_1",hist_str_case_1,"\n")
-                    hist_str = hist_strs[hist_str_case_1]
+                    hist_str = hist_strs#hist_strs[hist_str_case_1]
                 else:
                     #if isinstance(hist_strs, dict):
                     #    hist_str = hist_strs
