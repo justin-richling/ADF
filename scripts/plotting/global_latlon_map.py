19892164fa3769cebe05212029bfe337e0333ed7
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-08-20 16:16:02 -0600

    Update global_latlon_map.py

--- scripts/plotting/global_latlon_map.py
+++ scripts/plotting/global_latlon_map.py
@@ -535,6 +535,7 @@ def global_latlon_map(adfobj):
 
     #This will be a list of variables for multi-case plotting based off LatLon plot type
     if multi_plots:
+        print("multi_dict[var]",multi_dict[var])
         #Notify user that script has started:
         print("\n  Generating lat/lon multi-case plots...")
 
