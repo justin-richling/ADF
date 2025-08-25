3ac05db05940e5e0ebadb6988e4e140736db49d4
justin-richling <richling@ucar.edu>
2025-08-25 11:52:42 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -3447,7 +3447,7 @@ def multi_latlon_plots(wks, ptype, case_names, nicknames, multi_dict, web_catego
                     #Clean up the spacing a bit
                     #plt.subplots_adjust(wspace=0.3, hspace=hspace)
                     hspace = 0.2
-                    #plt.subplots_adjust(hspace=hspace)
+                    plt.subplots_adjust(hspace=hspace)
                     #plt.subplots_adjust(wspace=0.3)
 
                     fig.savefig(wks / file_name, bbox_inches='tight', dpi=300)
