78bf56be2dbc7c1d87442f2843cd36000770d963
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-08-29 17:05:38 -0600

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -178,7 +178,7 @@ class AdfDiag(AdfWeb):
 
         # Provide convenience functions for data handling:
         if not self.diag_var_list:
-            emsg = "No variables were specified in the 'diag_var_list' config argument."
+            emsg = f" {__file__} No variables were specified in the 'diag_var_list' config argument."
             #emsg += " Please add variables to this list"
             #emsg += "  ...NO CAM time series file generated."
             #self.end_diag_fail(emsg)
@@ -342,7 +342,7 @@ class AdfDiag(AdfWeb):
         # create copy of var list that can be modified for derivable variables
         diag_var_list = self.diag_var_list
         if not diag_var_list:
-            emsg = "No variables were specified in the 'diag_var_list' config argument."
+            emsg = f" {__file__} No variables were specified in the 'diag_var_list' config argument."
             #emsg += " Please add variables to this list"
             emsg += "  ...NO CAM time series file generated."
             #self.end_diag_fail(emsg)
