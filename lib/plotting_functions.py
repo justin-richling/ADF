a07847f77d1b8214cb3c58c00be58f4055d1e378
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-08-29 18:34:00 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -908,7 +908,7 @@ def make_polar_plot(wks, case_nickname, base_nickname,
         if unstructured:
             #configure for polycollection plotting
             #TODO, would be nice to have levels set from the info, above
-            ac = a.to_polycollection(projection=proj)
+            ac = a.to_polycollection()
             #ac.norm(norms[i])
             ac.set_cmap(cmap)
             ac.set_antialiased(False)
