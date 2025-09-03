0e3a2fb821aa791df7c55be058efa3dfde2620e3
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-09-03 09:53:54 -0600

    Update adf_config.py

--- lib/adf_config.py
+++ lib/adf_config.py
@@ -61,7 +61,7 @@ class AdfConfig(AdfBase):
         #nfil debug_fname = f"ADF_debug_{ext}.log"  -- self.__debug_fname
         #with open("adf_run_info.txt", "w") as f:
         #AdfBase.__debug_fname
-        with open(f"{AdfBase.__debug_fname}".replace("debug","run_info").replace(".log",".txt"), "w") as f:
+        with open(f"{AdfBase.debug_fname}".replace("debug","run_info").replace(".log",".txt"), "w") as f:
             f.write(str(config_file))
             f.write("-----------")
             branch = self.get_git_branch()
