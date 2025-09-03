06e527da7adb84caa64af8a7701dbf040745d72f
justin-richling <richling@ucar.edu>
2025-09-03 15:02:18 -0600

    Update polar_map.py

--- scripts/plotting/polar_map.py
+++ scripts/plotting/polar_map.py
@@ -74,12 +74,13 @@ def polar_map(adfobj):
     #read_config_var('multi_case_plots')
     if len(case_names) > 1:
         #Check if multi-plots are desired from yaml file
-        if adfobj.get_multi_case_info("polar_map"):
-            multi_plots = True
-            multi_dict = OrderedDict()
-            for multi_var in adfobj.get_multi_case_info("polar_map"):
-                if multi_var not in multi_dict:
-                    multi_dict[multi_var] = OrderedDict()
+        if "polar_map" in adfobj.get_multi_case_info:
+            if adfobj.get_multi_case_info("polar_map"):
+                multi_plots = True
+                multi_dict = OrderedDict()
+                for multi_var in adfobj.get_multi_case_info("polar_map"):
+                    if multi_var not in multi_dict:
+                        multi_dict[multi_var] = OrderedDict()
     #End if (check for multiple cases)
 
     """if multi_plots:
