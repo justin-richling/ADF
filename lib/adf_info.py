769c852dd8ecfff1d749046c38e3dce17c64b26e
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-09-08 12:12:52 -0600

    Update adf_info.py

--- lib/adf_info.py
+++ lib/adf_info.py
@@ -146,7 +146,7 @@ class AdfInfo(AdfConfig):
             active_env = "--"
 
         # Gather ADF run env info
-        log_name = self.debug_fname(self)
+        log_name = self.debug_fname()
         #Create new path object from user-specified plot directory path:
         plot_path = Path(self.plot_location[0])
 
