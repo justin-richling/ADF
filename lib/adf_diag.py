9a89c3dc7b6458df1469d0ddbb25af2b8427bcc8
justin-richling <richling@ucar.edu>
2025-09-11 10:12:59 -0600

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -890,7 +890,8 @@ class AdfDiag(AdfWeb):
 
                 # DOES NOT WORK CORRECTLY!
                 grid_ts = False
-                if grid_ts:
+                unstruct_plotting = self.unstructured_plotting
+                if unstruct_plotting:
                     # TEMPORARY: do a quick check if this on native grid and regrid
                     ts_0 = sorted(Path(ts_dir).glob("*.nc"))[0]
                     ts_file_ds = xr.open_dataset(ts_0)
