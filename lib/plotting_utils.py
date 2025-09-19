d99cdcfd6cd5bbd060c71341d6f1a4b37718f039
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-09-19 17:43:41 -0600

    Update plotting_utils.py

--- lib/plotting_utils.py
+++ lib/plotting_utils.py
@@ -544,7 +544,8 @@ def resolve_levels(adfobj, plotty, plot_type_dict, kwargs, polar_names):
         }
         contour_levels, contour_range, contour_linspace = key_map.get(plotty, (None, None, None))
 
-        def process_entry(entry, kind):
+        def process_entry(entry, kind, msg):
+            msg = f"{script_name}: resolve_levels(): process_entry()"
             """Handle lists and dicts for levels/ranges/linspace."""
             if isinstance(entry, list):
                 if len(entry) == 3:
