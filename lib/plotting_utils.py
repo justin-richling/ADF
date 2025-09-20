a03e8dbd6ff8cd9a312d452f25438d68e2843d41
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-09-19 18:56:05 -0600

    Update plotting_utils.py

--- lib/plotting_utils.py
+++ lib/plotting_utils.py
@@ -435,15 +435,11 @@ def try_load_ncl_cmap(adfobj, cmap_case):
         locfil = Path(".") / f"{cmap_case}.rgb"
         if locfil.is_file():
             data = read_ncl_colormap(adfobj, locfil)
-            print(type(data))
         else:
             try:
                 data = read_ncl_colormap(adfobj, url)
             except urllib.error.HTTPError:
                 msg += f"\n\tNCL colormap file not found"
-        """print(f"\n\tTry to overwrite from url:")
-        data = read_ncl_colormap(url)
-        print(type(data))"""
         if isinstance(data, np.ndarray):
             cm, cmr = ncl_to_mpl(adfobj, data, cmap_case)
             adfobj.debug_log(msg)
@@ -496,8 +492,6 @@ def get_cmap(adfobj, plotty, plot_type_dict, kwargs, polar_names):
     # NCL support
     if cmap_case in ncl_defaults:
         cmap_case = try_load_ncl_cmap(adfobj, cmap_case)
-
-    print(f"\n\tWHAT IUS HIS COLORMAP: {cmap_case}")
 
     # Final check: must exist in matplotlib or NCL
     if isinstance(cmap_case, str):
