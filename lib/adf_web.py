736767f3087b8e204cdd0c480c534a560231aaab
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-09-22 17:15:44 -0600

    Update adf_web.py

--- lib/adf_web.py
+++ lib/adf_web.py
@@ -656,7 +656,7 @@ class AdfWeb(AdfObs):
                     case1 = web_data.case
                     plot_types = plot_type_html
                 #End if
-
+                print("web_data",web_data)
                 print("web_data.ext",web_data.ext)
                 rend_kwarg_dict = {"title": main_title,
                                        "var_title": web_data.name,
