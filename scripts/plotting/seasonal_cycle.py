7077534e259cff8b64f6d0a69c749da6c8b1f1af
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-09-23 10:37:49 -0600

    Update seasonal_cycle.py

--- scripts/plotting/seasonal_cycle.py
+++ scripts/plotting/seasonal_cycle.py
@@ -765,6 +765,7 @@ def comparison_plots(plot_name, cam_var, case_names, case_nicknames, case_ds_dic
         # Find the next value below highest vertical level
         prev_major_tick = 10 ** (np.floor(np.log10(np.nanmin(data_lev))))
         y_lims = [float(lim) for lim in [1e3,prev_major_tick]]
+        print("y_lims",y_lims)
         ax.set_ylim(y_lims)
 
         plt.xticks(np.arange(-90,91,45),rotation=40)
