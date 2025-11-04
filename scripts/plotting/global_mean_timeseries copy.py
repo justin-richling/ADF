dc030007c452e0686814ae99f17817a8a0ddd57c
justin-richling <richling@ucar.edu>
2025-11-04 14:05:31 -0700

    Update global_mean_timeseries copy.py

--- scripts/plotting/global_mean_timeseries copy.py
+++ scripts/plotting/global_mean_timeseries copy.py
@@ -32,6 +32,7 @@ def global_mean_timeseries(adfobj):
     """
 
     #Notify user that script has started:
+    msg = "\n  Generating global mean time series plots..."
     print(f"{msg}\n  {'-' * (len(msg)-3)}")
 
     # Gather ADF configurations
