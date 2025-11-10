67fd35b96a8f2043895299d31d4c722515545060
justin-richling <richling@ucar.edu>
2025-11-10 10:39:30 -0700

    Update regrid_and_vert_interp.py

--- scripts/regridding/regrid_and_vert_interp.py
+++ scripts/regridding/regrid_and_vert_interp.py
@@ -113,6 +113,7 @@ def regrid_and_vert_interp(adf):
 
         #Extract model baseline variables:
         target_loc = adf.get_baseline_info("cam_climo_loc", required=True)
+        target_loc = adf.climo_locs_dict["baseline"][adf.data.ref_case_label]
         target_list = [adf.get_baseline_info("cam_case_name", required=True)]
     #End if
 
