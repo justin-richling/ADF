68e553dab4a64d4f94bae51c12a87d6d90a413a7
Justin Richling <richling@ucar.edu>
2026-04-06 09:58:44 -0600

    Remove `transform_first` - breaks polar plots

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -220,16 +220,16 @@ def make_polar_plot(adfobj, wks, case_nickname, base_nickname,
         img3 = ax3.contourf(lons, lats, pct_cyclic,  cmap=cmappct, norm=pctnorm, levels=levelspctdiff, extend=cp_info["extend"])
 
     if len(levs_pctdiff) < 2:
-        img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), colors="w", norm=pctnorm)
+        img3 = ax3.contourf(lons, lats, pct_cyclic, colors="w", norm=pctnorm)
         ax3.text(0.4, 0.4, empty_message, transform=ax3.transAxes, bbox=props)
     else:
-        img3 = ax3.contourf(lons, lats, pct_cyclic, transform=ccrs.PlateCarree(), cmap=cmappct, norm=pctnorm, levels=levelspctdiff, extend=cp_info["extend"])
+        img3 = ax3.contourf(lons, lats, pct_cyclic, cmap=cmappct, norm=pctnorm, levels=levelspctdiff, extend=cp_info["extend"])
 
     if len(levs_diff) < 2:
-        img4 = ax4.contourf(lons, lats, dif_cyclic, transform=ccrs.PlateCarree(), colors="w", norm=dnorm)
+        img4 = ax4.contourf(lons, lats, dif_cyclic, colors="w", norm=dnorm)
         ax4.text(0.4, 0.4, empty_message, transform=ax4.transAxes, bbox=props)
     else:
-        img4 = ax4.contourf(lons, lats, dif_cyclic, transform=ccrs.PlateCarree(), cmap=cmapdiff, norm=dnorm, levels=levelsdiff, extend=cp_info["extend_diff"])
+        img4 = ax4.contourf(lons, lats, dif_cyclic, cmap=cmapdiff, norm=dnorm, levels=levelsdiff, extend=cp_info["extend_diff"])
         
     #Set Main title for subplots:
     st = fig.suptitle(wks.stem[:-5].replace("_"," - "), fontsize=18)
